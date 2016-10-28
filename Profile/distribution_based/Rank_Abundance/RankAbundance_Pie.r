18154e565d5222ec366627d306ab28c49935ace4
kelvin2016 <lik2@upmc.edu>
2016-10-27 23:21:21 -0400

    updated to normalize before combining samples.  updated coloring

--- Profile/distribution_based/Rank_Abundance/RankAbundance_Pie.r
+++ Profile/distribution_based/Rank_Abundance/RankAbundance_Pie.r
@@ -2,8 +2,8 @@
 
 ###############################################################################
 
-progname <- commandArgs(FALSE)[4]
-args <- commandArgs(TRUE)
+progname = commandArgs(FALSE)[4]
+args = commandArgs(TRUE)
 
 arg_count=1
 
@@ -11,13 +11,25 @@ if(is.na(args[arg_count])){
 
 	script_name <- unlist(strsplit(progname,"="))[2]
 	usage <- paste (
-		"\nUsage:\n\t", script_name, "\n\t\t<Input summary_table.xls FileName>\n\n",
+		"\nUsage:\n\t", script_name, "\n\t\t<Input summary_table.tsv FileName>\n\n",
 		"Generates a pie chart\n",
 		"\n")
 
 	writeLines(usage)
-	writeLines("Input FileName not defined.\n")
+	writeLines("Input summary table FileName not defined.\n")
 	quit(status=0)
+}
+
+###############################################################################
+
+get_colors=function(num_col, alpha=1){
+        colors=hsv(seq(0,1,length.out=num_col+1), c(1,.5), c(1,.75,.5), alpha=alpha);
+        color_mat_dim=ceiling(sqrt(num_col));
+        color_pad=rep("grey", color_mat_dim^2);
+        color_pad[1:num_col]=colors[1:num_col];
+        color_mat=matrix(color_pad, nrow=color_mat_dim, ncol=color_mat_dim);
+        colors=as.vector(t(color_mat));
+        colors=colors[colors!="grey"];
 }
 
 ###############################################################################
@@ -30,39 +42,29 @@ while(!(is.na(args[arg_count]))){
 	OutputFileRoot=gsub("\\.summary_table\\.tsv$", "", OutputFileRoot);
 	OutputFileRoot=gsub("\\.summary_table\\.xls$", "", OutputFileRoot);
 
-	PiePDF= paste(OutputFileRoot, ".pie.pdf", sep="")
-	PieXLS= paste(OutputFileRoot, ".pie.xls", sep="")
+	PiePDF= paste(OutputFileRoot, ".pie.pdf", sep="");
+	PieXLS= paste(OutputFileRoot, ".pie.tsv", sep="");
 
-	cat("\n")
-	cat("Input File Name: ", InputFileName, "\n")
-	cat(" Pie Output PDF: ", PiePDF, "\n")
-	cat(" Pie Output XLS: ", PieXLS, "\n")
+	cat("\n");
+	cat("Input File Name: ", InputFileName, "\n");
+	cat(" Pie Output pdf: ", PiePDF, "\n");
+	cat(" Pie Output tsv: ", PieXLS, "\n");
+	cat("\n");
 
 	###############################################################################
 	###############################################################################
 
 	# Load data
-	mat<-as.matrix(read.table(InputFileName, sep="\t", header=TRUE, check.names=FALSE, row.names=1, comment.char=""));
+	mat=as.matrix(read.table(InputFileName, sep="\t", header=TRUE, check.names=FALSE, row.names=1, comment.char=""));
+	#print(mat);
 
-	print(mat);
+	count_mat=mat[,2:ncol(mat), drop=F];
 
-	print(dim(mat));
-
-	if(is.null(ncol(mat))){
-		mat=t(mat);
-	}
-	
-	count_mat=mat[,2:ncol(mat)];
-
-	if(is.null(ncol(count_mat))){
-		count_mat=t(count_mat);
-	}
-
-	cat("Rows: ", nrow(count_mat), "\n");
-	cat("Cols: ", ncol(count_mat), "\n");
-
-	categories=as.vector(colnames(count_mat));
-	num_categories=length(categories);
+	num_samples=nrow(count_mat);
+	num_categories=ncol(count_mat);
+	cat("Num Samples:    ", num_samples, "\n");
+	cat("Num Categories: ", num_categories, "\n");
+	categories=colnames(count_mat);
 
 	short_names=character(num_categories);
 	#for(i in 1:num_categories){
@@ -71,61 +73,54 @@ while(!(is.na(args[arg_count]))){
 	#}
 	short_names=categories;
 
-	#print(short_names);
+	###############################################################################
 
-	NumSamples=nrow(count_mat);
-	NumCategories=ncol(count_mat);
+	pdf(PiePDF, width=20, height=8.5)
 
-	cat("\n");
-	cat("Num Samples: ", NumSamples, "\n");
-	cat("Num Categories: ", NumCategories, "\n");
-	cat("\n");
-
-	pdf(PiePDF, width=11,height=8.5)
-
-	category_totals=numeric(NumCategories);
-	for(cat_idx in 1:NumCategories){
-		category_totals[cat_idx]=sum(count_mat[,cat_idx]);
+	# Normalized before combining
+	samp_tot=apply(count_mat, 1, sum);
+	norm_mat=count_mat;
+	for(samp_ix in 1:num_samples){
+		norm_mat[samp_ix,]=count_mat[samp_ix,]/samp_tot[samp_ix];
 	}
 
-	all_total=sum(category_totals);
-	
-	totals_sort=sort(category_totals, index.return=TRUE, decreasing=TRUE);
-	sorted_totals=totals_sort$x;
+	# Combine all samples
+	all_samp_norm=apply(norm_mat, 2, mean);
+	print(all_samp_norm);
+	order_ix=order(all_samp_norm, decreasing=T);
 
-	normalized=sorted_totals/all_total;
-	#print(normalized);
-	lt1p=normalized[normalized >= 0 & normalized < 0.01];
-	lt2p=normalized[normalized >= .01 & normalized < 0.02];
-	lt3p=normalized[normalized >= .02 & normalized < 0.03];
+	# Reorder values and categories names
+	all_samp_norm=all_samp_norm[order_ix];
+	short_names=short_names[order_ix];
+
+	# Combine small abundances to reduce clutter
+	lt1p=all_samp_norm[all_samp_norm >= 0 & all_samp_norm < 0.01];
+	lt2p=all_samp_norm[all_samp_norm >= .01 & all_samp_norm < 0.02];
+	lt3p=all_samp_norm[all_samp_norm >= .02 & all_samp_norm < 0.03];
 
 	lt1p_sum=sum(lt1p);
 	lt2p_sum=sum(lt2p);
 	lt3p_sum=sum(lt3p);
 
-	confined=normalized[normalized >=.03];
-	confined=c(confined,lt3p_sum,lt2p_sum,lt1p_sum);
-	
-	sorted_shortnames=short_names[totals_sort$ix];
-	confined_names=sorted_shortnames[normalized >=.03];
-	confined_names=c(confined_names, "2-3%", "1-2%", "<1%");
+	combined_val=all_samp_norm[all_samp_norm >=.03];
+	combined_val=c(combined_val, lt3p_sum, lt2p_sum, lt1p_sum);
 
-	labels=character(length(confined_names));
-	for(i in 1:length(confined_names)){
-		labels[i]=sprintf("%s (%1.1f%%)", confined_names[i], confined[i]*100);
-		print(labels[i]);
+	# Sort by abundance
+	combined_names=short_names[all_samp_norm >=.03];
+	combined_names=c(combined_names, "2-3%", "1-2%", "<1%");
+	num_slices=length(combined_names);
+
+	# Append percentages to labels
+	labels=character(num_slices);
+	for(i in 1:num_slices){
+		labels[i]=sprintf("%s (%1.1f%%)", combined_names[i], combined_val[i]*100);
+		cat("Slice Label [", i, "] : ", labels[i], "\n");
 	}
 	
-	cdf=numeric(length(confined_names));
-	cdf[1]=0;
-	for(i in 1:(length(confined_names)-1)){
-		cdf[i+1]=cdf[i]+confined[i];
-	}	
-	allcolors=rainbow(100, start=0, end=.65);
-	colors=allcolors[as.integer(cdf*100+1)];
+	colors=get_colors(num_slices);
 
-	pie(confined, labels, col=colors, init.angle=180, clockwise=TRUE,
-		main=InputFileName);
+	pie(combined_val, labels, col=colors, init.angle=180, clockwise=TRUE,
+		main=OutputFileRoot);
 
 	dev.off();
 
@@ -136,8 +131,8 @@ while(!(is.na(args[arg_count]))){
 	outline=paste("Category Name", "Percentage", sep=",");
 	write(outline, file=fc);
 
-	for(i in 1:length(confined)){
-		outline=paste(confined_names[i], sprintf("%.2f", confined[i]*100), sep=",");
+	for(i in 1:num_slices){
+		outline=paste(combined_names[i], sprintf("%.2f", combined_val[i]*100), sep=",");
 		write(outline, file=fc);
 	}
 
