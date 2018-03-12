0ffb4c9847002340e2ed1c47ce36420ddcc830d9
Li <lik2@upmc.edu>
2018-03-12 18:21:59 -0400

    modified to deal with headers in input or map file

--- Column/merge_by_map.pl
+++ Column/merge_by_map.pl
@@ -3,24 +3,32 @@
 ###############################################################################
 
 use strict;
+use File::Basename;
 use Getopt::Std;
-use vars qw ($opt_i $opt_m $opt_k $opt_h $opt_K $opt_s $opt_o $opt_v);
+use vars qw ($opt_i $opt_k $opt_h $opt_m $opt_K $opt_H $opt_s $opt_n $opt_o $opt_v);
 
-getopts("i:m:k:h:K:s:o:v");
+getopts("i:k:h:m:K:H:s:n:o:v");
 
 my $usage = "
 	usage:
 	$0
+		Input File Parameters:
 		-i <input file name, i.e. recipient file>
-		-m <map file name, i.e. donor file>
 		-k <key column in the input file, starting from 0>
-		-h <files contain header information, Y=yes, N=no>
+		-h <input files contain header information, Y=yes, N=no>
+		
+		Mapping File Parameters:
+		-m <map file name, i.e. donor file>
+		[-K <key column in the map file, starting from 0, default=0>]
+		-H <map file contains header information, Y=yes, N=no>
+
+		Merged Output File Parameters:
+		[-s <where to insert columns in input file, starting from 0, default=end>]
+		[-n <insert column name if there is header in input file, 
+			default: use header name in mapping file or filename]
 		-o <output file name>
 
 		Optional:
-		[-K <key column in the map file, starting from 0, default=0>]
-		[-s <where to insert columns in input file, starting from 0, default=end>]
-
 		[-v (show keys flag)]
 
 	This script will merge the input file with the map file.
@@ -39,25 +47,32 @@ my $usage = "
 
 if(
 	!defined($opt_i) || 
-	!defined($opt_m) || 
 	!defined($opt_k) || 
-	!defined($opt_o) ||
-	!defined($opt_h) 
+	!defined($opt_h) || 
+	!defined($opt_m) ||
+	!defined($opt_o) 
 ){
 	die $usage;
 }
 
 my $InputFile=$opt_i;
+my $InputFileKeyCol=$opt_k;
+my $InputFileHeader=uc($opt_h);
+
 my $MapFile=$opt_m;
-my $InputKeyCol=$opt_k;
-my $Header=uc($opt_h);
-my $MapKeyCol=$opt_K;
+my $MapFileKeyCol=$opt_K;
+my $MapFileHeader=uc($opt_H);
+
 my $InsertCol=$opt_s;
+my $OuputHeaderName=$opt_n;
 my $OutputFile=$opt_o;
+
 my $Verbose=defined($opt_v);
 
-if(!defined($MapKeyCol)){
-	$MapKeyCol=1;
+#------------------------------------------------------------------------------
+
+if(!defined($MapFileKeyCol)){
+	$MapFileKeyCol=0;
 }
 
 if(!defined($InsertCol)){
@@ -65,13 +80,18 @@ if(!defined($InsertCol)){
 }
 
 print STDERR "Input File: $InputFile\n";
-print STDERR "Map File: $MapFile\n";
-print STDERR "Output File: $OutputFile\n";
-print STDERR "Contains Header: $Header\n";
+print STDERR "Input File Key Column: $InputFileKeyCol\n";
+print STDERR "Input File Contains Header: $InputFileHeader\n";
 print STDERR "\n";
-print STDERR "Input Key Column: $InputKeyCol\n";
-print STDERR "Map Key Column: $MapKeyCol\n";
-print STDERR "Insertion Column: $InsertCol\n";
+print STDERR "Map File: $MapFile\n";
+print STDERR "Map File Key Column: $MapFileKeyCol\n";
+print STDERR "Map File Contains Header: $MapFileHeader\n";
+print STDERR "\n";
+print STDERR "Output Insertion Column: $InsertCol\n";
+print STDERR "Output Column Name: $OuputHeaderName\n";
+print STDERR "Output File Name: $OutputFile\n";
+print STDERR "\n";
+print STDERR "Verbose? $Verbose\n";
 print STDERR "\n";
 
 ###############################################################################
@@ -88,13 +108,13 @@ while(<MAP_FH>){
 	my @out_arr=@array;
 
 	# Remove key column
-	splice @out_arr, $MapKeyCol, 1;
+	splice @out_arr, $MapFileKeyCol, 1;
 	my $out_str=join "\t", @out_arr;
-	my $key=$array[$MapKeyCol];
+	my $key=$array[$MapFileKeyCol];
 
 	# Store header line separately
-	if($line==0 && $Header=="Y"){
-		$map_header=$out_str;
+	if($line==0 && $MapFileHeader=="Y"){
+		$map_header=$key;
 	}else{
 		$map_hash{$key}=$out_str;
 	}
@@ -102,6 +122,16 @@ while(<MAP_FH>){
 	$line++;
 }
 close(MAP_FH);
+
+if($map_header==""){
+	$map_header=fileparse($MapFile);
+}
+if(defined($OuputHeaderName)){
+	$map_header=$OuputHeaderName;
+}
+
+print STDERR "Output Column Name: $map_header\n";
+
 
 ###############################################################################
 
@@ -111,6 +141,9 @@ if($Verbose){
 		my $num_out=10;
 		for(my $i=0; $i<$num_out; $i++){
 			print STDERR "${$arr_ref}[$i]\n";
+		}
+		if(($#{$arr_ref}+1)>$num_out){
+			print STDERR "...\n";
 		}
 	}
 
@@ -123,16 +156,17 @@ if($Verbose){
 
 	# Output File Keys
 	my @in_keys;
-	open(IN_FH, "head -n 10 $InputFile |") || die "Could not open $InputFile\n";
+	open(IN_FH, "head -n 11 $InputFile |") || die "Could not open $InputFile\n";
 	while(<IN_FH>){
 		chomp;
 		my @arr=split /\t/, $_;
-		push @in_keys, $arr[$InputKeyCol];
+		push @in_keys, $arr[$InputFileKeyCol];
 	}
 	close(IN_FH);
 	
 	print STDERR "Input Keys:\n";
 	print_arr(\@in_keys);
+	print STDERR "\n";
 
 }
 
@@ -141,6 +175,7 @@ if($Verbose){
 open(INPUT_FH, "<$InputFile") || die "Could not open input file: $InputFile\n";
 open(OUTPUT_FH, ">$OutputFile") || die "Could not open output file: $OutputFile\n";
 
+my %unmapped_hash;
 print STDERR "Iterating through input file...\n";
 my $line=0;
 while(<INPUT_FH>){
@@ -148,20 +183,26 @@ while(<INPUT_FH>){
 	my @array=split /\t/, $_, -1;
 	my @out_array=@array;
 
+	# By default, insert column at end
 	if(!defined($InsertCol)){
 		$InsertCol=$#array+1;
 	}
 
-	if($line==0 && $Header=="Y"){
+	if($line==0 && $InputFileHeader=="Y"){
 		# Output header with new columns inserted in
+		print STDERR "Inserting $map_header into column $InsertCol...\n";
 		splice @out_array, $InsertCol, 0, $map_header;
 	}else{
 
-		my $key=$array[$InputKeyCol];
+		my $key=$array[$InputFileKeyCol];
 		my $map_val=$map_hash{$key};
 
 		if(!defined($map_val)){
 			$map_val="UNDEFINED";			
+			if($Verbose && !defined($unmapped_hash{$key})){
+				$unmapped_hash{$key}=1;
+				print STDERR "No Mapping for: $key\n";
+			}
 		}
 
 		splice @out_array, $InsertCol, 0, $map_val;
