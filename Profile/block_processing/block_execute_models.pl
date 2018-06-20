d7ae80f9cbce2f507d955d7276919c6765d3a1cd
Li <lik2@upmc.edu>
2018-06-20 15:28:48 -0400

    added distance based methods, added another layer of directories, tweaked checkpoint file

--- Profile/block_processing/block_execute_models.pl
+++ Profile/block_processing/block_execute_models.pl
@@ -49,7 +49,6 @@ my $usage = "
 			Cluster MLL
 
 
-	The following code will be executed:
 
 ";
 
@@ -115,22 +114,29 @@ sub run_command{
 
 	# Check if command needs to be executed
 	if(-e $checkpt_file){
-		open(CHK_FH, "<$checkpt_file") || die "Could not open $checkpt_file.\n";
-		my $first_line=<CHK_FH>;
+		open(CHK_FH, "tail -n 1 $checkpt_file |") || die "Could not open $checkpt_file.\n";
+		my $last_line=<CHK_FH>;
 		close(CHK_FH);
 		
-		$first_line=~s/\s+//g;
-		if($first_line eq "Completed."){
+		$last_line=~s/\s+//g;
+		if($last_line eq "Completed."){
 			print STDERR "$cmd_name ($checkpt_file) was successfully completed previous. Skipping.\n";
 			return;
 		}
 	}
 
-	# Execute command
+	# Prep command
 	my $clean_cmd=$cmd_str;
-	print STDERR "'$cmd_str'\n";
 	$clean_cmd=~s/\s+/ /g;
+
 	print STDERR "'$clean_cmd'\n";
+	
+	# Save command to file
+	open(CHK_FH, ">$checkpt_file") || die "Could not open $checkpt_file.\n";
+	print CHK_FH "$clean_cmd\n";
+	close(CHK_FH);
+
+	# Execute command
 	my $result=`$clean_cmd`;
 	my $exit_code=$?;
 
@@ -140,12 +146,17 @@ sub run_command{
 	print LOG_FH "$result";
 	close(LOG_FH);
 
-	# Write checkpoint file
+	# Write status to end of checkpoint file
 	if($exit_code==0){
-		open(CHK_FH, ">$checkpt_file") || die "Could not open $checkpt_file.\n";
-		print CHK_FH "Completed.\n";
+		open(CHK_FH, ">>$checkpt_file") || die "Could not open $checkpt_file.\n";
+		print CHK_FH "\nCompleted.\n";
+		close(CHK_FH);
 		return;
 	}else{
+		open(CHK_FH, ">>$checkpt_file") || die "Could not open $checkpt_file.\n";
+		print CHK_FH "\nFailed.\n";
+		close(CHK_FH);
+
 		print STDERR "Error: $cmd_name returned with non-zero error code.\n";
 		print STDERR "\n";
 		print STDERR "$cmd_str\n";
@@ -176,9 +187,10 @@ sub run_abundance_based{
 	$cmd="cat $covariates $variable_list > $output_dir/cov_var";
 	run_command("Concatenate variables into full model list", "concat", $cmd, $output_dir);
 
-	mkdir "$output_dir/$RESP_OUT_DIR";
-	mkdir "$output_dir/$PRED_OUT_DIR";
-	mkdir "$output_dir/$COMP_DIR";
+	mkdir "$output_dir/abundance";
+	mkdir "$output_dir/abundance/$RESP_OUT_DIR";
+	mkdir "$output_dir/abundance/$PRED_OUT_DIR";
+	mkdir "$output_dir/abundance/$COMP_DIR";
 
 	
 	$cmd=
@@ -188,10 +200,10 @@ sub run_abundance_based{
                 -M $output_dir/cov_var \
                 -q $output_dir/cov_var \
                 -p $num_alr \
-                -o $output_dir/$RESP_OUT_DIR/$model_name  \
+                -o $output_dir/abundance/$RESP_OUT_DIR/$model_name  \
                 -x \";\"
 	";
-	run_command("Fit ALR as Response", "alr_as_resp", $cmd, $output_dir);
+	run_command("Fit ALR as Response", "alr_as_resp", $cmd, "$output_dir/abundance/$RESP_OUT_DIR");
 
 	$cmd=
         "~/git/AnalysisTools/Profile/abundance_based/Fit_ALR_as_Predictor/Fit_ALR_as_Predictor.r \
@@ -201,22 +213,22 @@ sub run_abundance_based{
                 -c $covariates \
                 -q $output_dir/cov_var \
                 -p $num_alr \
-                -o $output_dir/$PRED_OUT_DIR/$model_name \
+                -o $output_dir/abundance/$PRED_OUT_DIR/$model_name \
                 -x \";\"
 	";
-	run_command("Fit ALR as Predictor", "alr_as_pred", $cmd, $output_dir);
+	run_command("Fit ALR as Predictor", "alr_as_pred", $cmd, "$output_dir/abundance/$PRED_OUT_DIR");
 
 
 	$cmd=
       	"~/git/AnalysisTools/Profile/abundance_based/Compare_ALR_PredResp/Compare_ALR_PredResp.r \
-                -x $output_dir/$PRED_OUT_DIR/$model_name.alr_as_pred.pvals.tsv \
-                -y $output_dir/$RESP_OUT_DIR/$model_name.alr_as_resp.pvals.tsv \
-                -u $output_dir/$PRED_OUT_DIR/$model_name.alr_as_pred.coefs.tsv \
-                -v $output_dir/$RESP_OUT_DIR/$model_name.alr_as_resp.coefs.tsv \
-                -o $output_dir/$COMP_DIR/$model_name.alr \
+                -x $output_dir/abundance/$PRED_OUT_DIR/$model_name.alr_as_pred.pvals.tsv \
+                -y $output_dir/abundance/$RESP_OUT_DIR/$model_name.alr_as_resp.pvals.tsv \
+                -u $output_dir/abundance/$PRED_OUT_DIR/$model_name.alr_as_pred.coefs.tsv \
+                -v $output_dir/abundance/$RESP_OUT_DIR/$model_name.alr_as_resp.coefs.tsv \
+                -o $output_dir/abundance/$COMP_DIR/$model_name.alr \
                 -p .025
 	";
-	run_command("Compare ALR Pred/Resp", "alr_pred_resp_comp", $cmd, $output_dir);	
+	run_command("Compare ALR Pred/Resp", "alr_pred_resp_comp", $cmd, "$output_dir/abundance/$COMP_DIR");	
 
 }
 
@@ -240,9 +252,10 @@ sub run_distribution_based{
 	$cmd="cat $covariates $variable_list > $output_dir/cov_var";
 	run_command("Concatenate variables into full model list", "concat", $cmd, $output_dir);
 
-	mkdir "$output_dir/$RESP_OUT_DIR";
-	mkdir "$output_dir/$PRED_OUT_DIR";
-	mkdir "$output_dir/$COMP_DIR";
+	mkdir "$output_dir/distribution";
+	mkdir "$output_dir/distribution/$RESP_OUT_DIR";
+	mkdir "$output_dir/distribution/$PRED_OUT_DIR";
+	mkdir "$output_dir/distribution/$COMP_DIR";
 
 	$cmd=
 	 "~/git/AnalysisTools/Profile/distribution_based/Fit_Diversity_as_Response/Fit_Diversity_as_Response.r \
@@ -250,9 +263,9 @@ sub run_distribution_based{
                 -f $factor_file \
                 -M $output_dir/cov_var \
                 -q $output_dir/cov_var \
-             	-o $output_dir/$RESP_OUT_DIR/$model_name 
+             	-o $output_dir/distribution/$RESP_OUT_DIR/$model_name 
 	";
-	run_command("Fit Diversity as Response", "div_as_resp", $cmd, $output_dir);
+	run_command("Fit Diversity as Response", "div_as_resp", $cmd, "$output_dir/distribution/$RESP_OUT_DIR");
 
 	$cmd=
         "~/git/AnalysisTools/Profile/distribution_based/Fit_Diversity_as_Predictor/Fit_Diversity_as_Predictor.r \
@@ -261,20 +274,20 @@ sub run_distribution_based{
                 -y $variable_list \
                 -c $covariates \
                 -q $output_dir/cov_var \
-                -o $output_dir/$PRED_OUT_DIR/$model_name
+                -o $output_dir/distribution/$PRED_OUT_DIR/$model_name
 	";
-	run_command("Fit Diversity as Predictor", "div_as_pred", $cmd, $output_dir);
+	run_command("Fit Diversity as Predictor", "div_as_pred", $cmd, "$output_dir/distribution/$PRED_OUT_DIR");
 
 	$cmd=
         "~/git/AnalysisTools/Profile/abundance_based/Compare_ALR_PredResp/Compare_ALR_PredResp.r \
-                -x $output_dir/$PRED_OUT_DIR/$model_name.div_as_pred.pvals.tsv \
-                -y $output_dir/$RESP_OUT_DIR/$model_name.div_as_resp.pvals.tsv \
-                -u $output_dir/$PRED_OUT_DIR/$model_name.div_as_pred.coefs.tsv \
-                -v $output_dir/$RESP_OUT_DIR/$model_name.div_as_resp.coefs.tsv \
-                -o $output_dir/$COMP_DIR/$model_name.div \
+                -x $output_dir/distribution/$PRED_OUT_DIR/$model_name.div_as_pred.pvals.tsv \
+                -y $output_dir/distribution/$RESP_OUT_DIR/$model_name.div_as_resp.pvals.tsv \
+                -u $output_dir/distribution/$PRED_OUT_DIR/$model_name.div_as_pred.coefs.tsv \
+                -v $output_dir/distribution/$RESP_OUT_DIR/$model_name.div_as_resp.coefs.tsv \
+                -o $output_dir/distribution/$COMP_DIR/$model_name.div \
                 -p .025
 	";
-	run_command("Compare Diversity Pred/Resp", "div_pred_resp_comp", $cmd, $output_dir);
+	run_command("Compare Diversity Pred/Resp", "div_pred_resp_comp", $cmd, "$output_dir/distribution/$COMP_DIR");
 
 
 }
@@ -295,43 +308,84 @@ sub run_distance_based{
 	my $PERMA_DIR="permanova";
 	my $CLUST_MLL_DIR="clust_mll";
 	my $cmd;
+	my $dist_type="man";
 
 
 	$cmd="cat $covariates $variable_list > $output_dir/cov_var";
 	run_command("Concatenate variables into full model list", "concat", $cmd, $output_dir);
 
-	mkdir "$output_dir/$DISTMAT_DIR";
-	mkdir "$output_dir/$PERMA_DIR";
-	mkdir "$output_dir/$CLUST_MLL_DIR";
+	mkdir "$output_dir/distance";
+	mkdir "$output_dir/distance/$DISTMAT_DIR";
+	mkdir "$output_dir/distance/$PERMA_DIR";
+	mkdir "$output_dir/distance/$CLUST_MLL_DIR";
 
 	my ($st_root_name)=File::Basename::fileparse($summary_table);
 	$st_root_name=~s/\.summary_table\.tsv$//;
 
+	#######################################################################
+	# Compute distance matrix
 	$cmd=
 	"~/git/AnalysisTools/Profile/SummaryTableUtilities/Create_Distance_Matrix.r \
                 -i $summary_table \
-                -d man \
-             	-o $output_dir/$DISTMAT_DIR/$st_root_name
+                -d $dist_type \
+             	-o $output_dir/distance/$DISTMAT_DIR/$st_root_name
 	";
-	run_command("Compute Distance Matrix", "dist_mat", $cmd, $output_dir);
+	run_command("Compute Distance Matrix", "dist_mat", $cmd, "$output_dir/distance/$DISTMAT_DIR");
 
+	# Compute permanova
 	$cmd=
 	"~/git/AnalysisTools/Profile/distance_based/Permanova/Permanova.r \
-                -d $output_dir/$DISTMAT_DIR/$st_root_name.man.distmat \
+                -d $output_dir/distance/$DISTMAT_DIR/$st_root_name.man.distmat \
                 -f $factor_file \
 		-M $output_dir/cov_var \
-		-o $output_dir/$PERMA_DIR/$model_name \
+		-o $output_dir/distance/$PERMA_DIR/$model_name \
 		-q $output_dir/cov_var
 	";
-	run_command("Permanova", "perma", $cmd, $output_dir);
+	run_command("Permanova", "perma", $cmd, "$output_dir/distance/$PERMA_DIR");
 		
 
+	#######################################################################
+	# Compute cluster multinomial log linear model
+	$cmd=
+	"~/git/AnalysisTools/Profile/distance_based/Cluster_MultinomLogLin/Cluster_MultinomLogLin.r \
+		-i $summary_table \
+		-f $factor_file \
+		-M $output_dir/cov_var \
+		-q $output_dir/cov_var \
+		-o $output_dir/distance/$CLUST_MLL_DIR/$model_name \
+		-d $dist_type
+	";
+	run_command("Fit Multinomial Log-Linear Model", "clust_mll", $cmd, "$output_dir/distance/$CLUST_MLL_DIR");
 
-	#compute distance matrix
+	$cmd=
+	"~/git/AnalysisTools/Profile/distance_based/Plot_ClusterHeatmap/Plot_ClusterHeatmap.r \
+		-i $summary_table \
+		-f $factor_file \
+		-d $dist_type \
+		-M $output_dir/cov_var \
+		-c $output_dir/distance/$CLUST_MLL_DIR/$model_name\.$dist_type\.cl_mll.used_samp.lst \
+		-o $output_dir/distance/$CLUST_MLL_DIR/$model_name
+	";
+	run_command("Plot Metadata Heatmap", "cl_hmp", $cmd, "$output_dir/distance/$CLUST_MLL_DIR");
 
-	#permanova
+	$cmd=
+	"~/git/AnalysisTools/Profile/distance_based/CH_Cluster_Stop/CH_Cluster_Stop.r \
+		-i $summary_table \
+		-d $dist_type \
+		-c $output_dir/distance/$CLUST_MLL_DIR/$model_name\.$dist_type\.cl_mll.used_samp.lst \
+		-o $output_dir/distance/$CLUST_MLL_DIR/$model_name
+	";
+	run_command("Compute Calinski-Harabasz Stopping", "ch_stop", $cmd, "$output_dir/distance/$CLUST_MLL_DIR");
 
-	#cluster_mll
+	$cmd=
+	"~/git/AnalysisTools/Profile/distance_based/Cluster_Influencers/Cluster_Influencers.r \
+		-i $summary_table \
+		-d $dist_type \
+		-l $output_dir/distance/$CLUST_MLL_DIR/$model_name\.$dist_type\.cl_mll.used_samp.lst \
+		-o $output_dir/distance/$CLUST_MLL_DIR/$model_name
+	";
+	run_command("Compute Cluster Influencers", "cl_inf", $cmd, "$output_dir/distance/$CLUST_MLL_DIR");
+
 
 }
 
