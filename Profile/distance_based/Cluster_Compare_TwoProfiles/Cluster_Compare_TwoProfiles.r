91bd25141d4560723707e3c004a68bf75ea247c0
Li <lik2@upmc.edu>
2018-12-10 17:57:02 -0500

    added more plots

--- Profile/distance_based/Cluster_Compare_TwoProfiles/Cluster_Compare_TwoProfiles.r
+++ Profile/distance_based/Cluster_Compare_TwoProfiles/Cluster_Compare_TwoProfiles.r
@@ -329,25 +329,29 @@ plot_text(param_summary);
 
 ###############################################################################
 
-if(0){
-cat("Bootstrapping Mantel Correlation...\n");
-mantel_res=mantel(dist_mat_A, dist_mat_B, permutations=10000);
-print(mantel_res);
+test=1;
 
-mantel_cor=mantel_res[["statistic"]];
-mantel_pval=mantel_res[["signif"]];
-cat("\n");
-cat("Correlation: ", mantel_cor, " ", "P-val: ", mantel_pval, "\n");
-cat("\n");
+###############################################################################
 
-plot_text(c(
-	"Mantel Test based on Pearson's Product-Moment Correlation:",
-	paste("Correlation:", mantel_cor, sep=""),
-	paste("    P-value:", mantel_pval, sep=""),
-	"",
-	"Note: Correl Range is -1.0 to 1.0, and Null Hypothesis is 0.0 correlation."
+if(!test){
+	cat("Bootstrapping Mantel Correlation...\n");
+	mantel_res=mantel(dist_mat_A, dist_mat_B, permutations=10000);
+	print(mantel_res);
 
-));
+	mantel_cor=mantel_res[["statistic"]];
+	mantel_pval=mantel_res[["signif"]];
+	cat("\n");
+	cat("Correlation: ", mantel_cor, " ", "P-val: ", mantel_pval, "\n");
+	cat("\n");
+
+	plot_text(c(
+		"Mantel Test based on Pearson's Product-Moment Correlation:",
+		paste("Correlation:", mantel_cor, sep=""),
+		paste("    P-value:", mantel_pval, sep=""),
+		"",
+		"Note: Correl Range is -1.0 to 1.0, and Null Hypothesis is 0.0 correlation."
+
+	));
 }
 
 ##############################################################################
@@ -385,26 +389,38 @@ compute_pseudof=function(clsmem, distmat){
 	num_samples=length(clsmem);
 	distmat2d=as.matrix(distmat);
 
-	ssw=0;
-	ssb=0;
-
-	nsw=0;
+	tsw=0;
+	tsb=0;
 	nsb=0;
+	nsw=0;
 
 	for(i in 1:num_grps){
 		grp_ix=(clsmem==i);
-		for(j in 2:num_grps){
+		for(j in 1:num_grps){
 			grp_jx=(clsmem==j);
 
-			dst=as.dist(distmat2d[grp_ix, grp_jx]);
-			ssd=sum(dst^2);
-			num_dist=length(dst);
-			
+			subdist=distmat2d[grp_ix, grp_jx];
+
 			if(i==j){
-				tsw=ssw+ssd;
+				
+				# cat("SSW:\n");
+				# print(dim(subdist));
+				ssd=sum(subdist^2)/2;
+
+				rows=nrow(subdist);
+				num_dist=rows*(rows-1)/2;	# Number of distances in half matrix	
+				
+				tsw=tsw+ssd;
 				nsw=nsw+num_dist;
 			}else{
-				tsb=ssb+ssd;
+
+				# cat("SSB:\n");
+				# print(dim(subdist));
+				ssd=sum(subdist^2);
+
+				num_dist=nrow(subdist)*ncol(subdist);
+
+				tsb=tsb+ssd;
 				nsb=nsb+num_dist;
 			}
 
@@ -420,7 +436,11 @@ compute_pseudof=function(clsmem, distmat){
 	w_var=ssw/(num_samples-num_grps);
 
 	# Return pseudo F-stat
-	return(b_var/w_var);
+	pseudof=b_var/w_var;
+	if(!length(pseudof)){
+		pseudof=0;
+	}
+	return(pseudof);
 
 }
 
@@ -443,16 +463,18 @@ compare_pseudof=function(dista, distb, grp_hcla, grp_hclb, max_k, namea, nameb){
 		bmsd_byB[clix-1]=compute_pseudof(mem_byB, distb);
 	}
 
-	max_asmd=max(amsd_byA, amsd_byB);
-	max_bsmd=max(bmsd_byA, bmsd_byB);
-	min_asmd=min(amsd_byA, amsd_byB);
-	min_bsmd=min(bmsd_byA, bmsd_byB);
+	max_asmd=max(amsd_byA, amsd_byB, na.rm=T);
+	max_bsmd=max(bmsd_byA, bmsd_byB, na.rm=T);
+	min_asmd=min(amsd_byA, amsd_byB, na.rm=T);
+	min_bsmd=min(bmsd_byA, bmsd_byB, na.rm=T);
 
 	asmd_range=c(min_asmd, max_asmd);
 	bsmd_range=c(min_bsmd, max_bsmd);
 
 	par(mfrow=c(2,3));
 
+
+	# Clustering from A's perspective
 	plot(2:max_k, amsd_byA, xlab="Num Clusters, k", ylab="Pseudo F-Stat", main=paste("Pseudo F-Stat: ", namea, sep=""), type="b", ylim=asmd_range);
 	mtext(paste("Clustered by Optimal ", namea, " Groupings", sep=""), cex=.6);
 
@@ -460,15 +482,14 @@ compare_pseudof=function(dista, distb, grp_hcla, grp_hclb, max_k, namea, nameb){
 	mtext(paste("Clustered by Optimal ", nameb, " Groupings", sep=""), cex=.6);
 
 	lograt=log(amsd_byB/amsd_byA);
-	lims=c(-1,1)*max(abs(lograt));
+	lograt[!is.finite(lograt)]=0;
+	lims=c(-1,1)*max(abs(lograt), rm.na=T);
 	plot(2:max_k, lograt, xlab="Num Clusters, k", ylab=paste("Pseudo F-Stat LogRatio(", namea, ")", sep=""),  main=paste("Pseudo F-stat Ratio"), type="b", ylim=lims);
 	abline(h=0, col="blue");
 	mtext(paste(namea, ": By ", nameb, "/", namea, " Groupings", sep=""), cex=.6);
 
 
-
-
-
+	# Clustering from B's perspective
 	plot(2:max_k, bmsd_byB, xlab="Num Clusters, k", ylab="Pseudo F-Stat", main=paste("Pseudo F-stat: ", nameb, sep=""), type="b", ylim=bsmd_range);
 	mtext(paste("Clustered by Optimal ", nameb, " Groupings", sep=""), cex=.6);
 
@@ -476,7 +497,8 @@ compare_pseudof=function(dista, distb, grp_hcla, grp_hclb, max_k, namea, nameb){
 	mtext(paste("Clustered by Optimal ", namea, " Groupings", sep=""), cex=.6);
 
 	lograt=log(bmsd_byA/bmsd_byB);
-	lims=c(-1,1)*max(abs(lograt));
+	lograt[!is.finite(lograt)]=0;
+	lims=c(-1,1)*max(abs(lograt), rm.na=T);
 	plot(2:max_k, lograt, xlab="Num Clusters, k", ylab=paste("Pseudo F-Stat LogRatio(", nameb, ")", sep=""),  main=paste("Pseudo F-stat Ratio"), type="b", ylim=lims);
 	abline(h=0, col="blue");
 	mtext(paste(nameb, ": By ", namea, "/", nameb, " Groupings", sep=""), cex=.6);
@@ -484,11 +506,85 @@ compare_pseudof=function(dista, distb, grp_hcla, grp_hclb, max_k, namea, nameb){
 
 }
 
-compare_dendrograms=function(hclA, hclB){
+compare_dendrograms=function(hclA, hclB, num_cuts, namea, nameb, idsb){
 
+	color_denfun_bySample=function(n){
+		if(is.leaf(n)){
+			leaf_attr=attributes(n);
+			leaf_name=leaf_attr$label;
+			ind_color=sample_to_color_map[leaf_name];
+			if(is.null(ind_color)){
+				ind_color="black";
+			}
+
+			attr(n, "nodePar") = c(leaf_attr$nodePar,
+							list(lab.col=ind_color));
+		}
+		return(n);
+	}
+
+	text_scale_denfun=function(n){
+		if(is.leaf(n)){
+			leaf_attr=attributes(n);
+			leaf_name=leaf_attr$label;
+			attr(n, "nodePar") = c(leaf_attr$nodePar,
+						cex=0,
+						lab.cex=label_scale);
+		}
+		return(n);
+	}
+
+	orig_par=par(no.readonly=T);
+
+	par(mar=c(10,2,3,.5));
 	par(mfrow=c(2,1));
-	plot(hclA);
-	plot(hclB);
+
+	dendra=as.dendrogram(hclA);
+	dendrb=as.dendrogram(hclB);
+
+	label_scale=.2;
+	dendra=dendrapply(dendra, text_scale_denfun);
+	dendrb=dendrapply(dendrb, text_scale_denfun);
+
+	mem_byA=cutree(hclA, k=num_cuts);
+	sample_to_color_map=mem_byA;
+	dendra=dendrapply(dendra, color_denfun_bySample);
+	names(sample_to_color_map)=idsb;
+	dendrb=dendrapply(dendrb, color_denfun_bySample);
+	
+
+	plot(dendra, main=paste("Cut by ", namea, "'s clustering", sep=""));
+	plot(dendrb, main=nameb);
+
+	# Plot shared statistics
+	
+	shared_mat=matrix(0, nrow=num_cuts, ncol=num_cuts);
+	samples_a=names(mem_byA);
+
+	mem_byB=cutree(hclB, k=num_cuts);
+	names(mem_byB)=names(mem_byA);
+
+	num_samples=length(mem_byA);
+	cat("Num Samples: ", num_samples, "\n", sep="");
+
+	for(r in 1:num_cuts){
+
+		samples_in_r=samples_a[(mem_byA==r)];
+
+		for(c in 1:num_cuts){
+		
+			total=sum(mem_byB==c);
+			samples_in_c=(mem_byB[samples_in_r]==c);
+			overlapping=sum(mem_byB[samples_in_r]==c);
+
+			shared_mat[r, c]=overlapping/num_samples;
+		}
+	}
+	
+	print(shared_mat);
+
+	par(orig_par);
+
 
 
 }
@@ -524,7 +620,10 @@ compare_mds(nonparm_mds_pts_A, nonparm_mds_pts_B, "NonMetric MDS", clus4_A, clus
 
 ##############################################################################
 
-compare_dendrograms(hcl_A, hcl_B);
+for(cix in 2:cuts){
+	compare_dendrograms(hcl_A, hcl_B, cix, map_info[["a"]], map_info[["b"]], map_info[["b_id"]]);
+	compare_dendrograms(hcl_B, hcl_A, cix, map_info[["b"]], map_info[["a"]], map_info[["a_id"]]);
+}
 
 ##############################################################################
 
@@ -541,9 +640,11 @@ par(mfrow=c(2,2));
 hist(dist_mat_A, xlab="Distances", main=map_info[["a"]]);
 hist(dist_mat_B, xlab="Distances", main=map_info[["b"]]);
 
-cat("Computing 2D histogram/Heatmap...\n");
-k=kde2d(dist_mat_A, dist_mat_B, n=500);
-image(k, col=rev(rainbow(100, start=0, end=4/6)), xlab=map_info[["a"]], ylab= map_info[["b"]], main=sprintf("Correlation: %3.3f", cor(dist_mat_A, dist_mat_B)));
+if(!test){
+	cat("Computing 2D histogram/Heatmap...\n");
+	k=kde2d(dist_mat_A, dist_mat_B, n=500);
+	image(k, col=rev(rainbow(100, start=0, end=4/6)), xlab=map_info[["a"]], ylab= map_info[["b"]], main=sprintf("Correlation: %3.3f", cor(dist_mat_A, dist_mat_B)));
+}
 
 max_dist_A=max(dist_mat_A);
 max_dist_B=max(dist_mat_B);
@@ -553,68 +654,86 @@ plot(dist_mat_A, dist_mat_B, main="All Distances", xlab=map_info[["a"]], ylab= m
 # Subsample so we can differentiate all the points in the scatter plot
 num_dist=length(dist_mat_A);
 
-if(0){
-MAXSAMP=500;
-samp_size=min(num_dist, MAXSAMP);
-
-if(samp_size<=MAXSAMP){
-	subsamp_ix=1:samp_size;
-}else{
-	subsamp_ix=sample(1:num_dist, MAXSAMP, replace=F);
-}
-
-plot(dist_mat_A[subsamp_ix], dist_mat_B[subsamp_ix], main=paste(samp_size, " Distances", sep=""), 
-	xlab=map_info[["a"]], ylab= map_info[["b"]], cex=.5, xlim=c(0, max_dist_A), ylim=c(0, max_dist_B));
-}
-
 ##############################################################################
 
+cat("Analyze sub-cluster statistics\n");
 
-print(mean(dist_mat_A));
-print(mean(dist_mat_B));
+analyze_subclusters=function(hclA, hclB, distmatA, distmatB, num_cuts, namea, nameb, idsb){
 
-print(median(dist_mat_A));
-print(median(dist_mat_B));
+	# Mean sum of squared distances 
+	msd=function(dist){
+		# Square matrix
+		num_samp=ncol(dist);
+		num_dist=(num_samp*(num_samp-1));
+		#cat("Num Dist: ", num_dist, "\n");
+		msd=sum(dist^2)/num_dist;
+		return(msd);
+	}
 
-quit();
+	# Spearman correlation
+	distcorel=function(dista, distb){
+		return(cor(dista, distb, method="spearman"));	
+	}
 
-hcl=hclust(full_dist_mat, method="ward.D2");
+	dist2da=as.matrix(distmatA);
+	dist2db=as.matrix(distmatB);
 
-# Find height where cuts are made
-max_clusters=ceiling(log(num_samples, 2));
-cat("Max Clusters to compute: ", max_clusters, "\n");
-cut_midpoints=numeric(max_clusters);
-for(k in 2:max_clusters){
-        cut_midpoints[k]=find_height_at_k(hcl, k);
+	msd_mat_a=matrix(NA, nrow=num_cuts, ncol=num_cuts);
+	msd_mat_b=matrix(NA, nrow=num_cuts, ncol=num_cuts);
+
+	for(clx in 1:num_cuts){
+		cat("Analyzing ", clx, " cuts to: ", namea, "\n");
+		memA=cutree(hclA, k=clx);
+		
+		#memB=memA;
+		#names(memB)=idsb;
+
+		for(clid in 1:clx){
+			cat("	Cluster ", clid, " of ", clx, "\n");
+			memids=(memA==clid);
+
+			cl_dista=dist2da[memids, memids];
+			cl_distb=dist2db[memids, memids];
+
+			
+			arange=range(cl_dista);
+			
+			msdA=msd(cl_dista);
+			msdB=msd(cl_distb);
+
+			cor=distcorel(as.dist(cl_dista), as.dist(cl_distb));
+		
+			cat("  MSD A: ", msdA, ", Range: ", arange[1], "-", arange[2], " MSD B: ", msdB, "    Cor: ", cor, "\n");
+
+			msd_mat_a[clx, clid]=msdA;
+			msd_mat_b[clx, clid]=msdB;
+			
+		}
+
+	}
+
+	par(mfrow=c(2,1));
+	maxmsd=max(msd_mat_a, na.rm=T);
+	plot(0,0, type="n", xlim=c(1, num_cuts), ylim=c(0, maxmsd), xlab="Cluster Cuts", ylab="MSD", main=paste(namea, " clustered by ", namea, sep=""));
+	abline(h=msd_mat_a[1,1], col="blue", lty=2);
+	for(clx in 1:num_cuts){
+		points(rep(clx, clx), msd_mat_a[clx, 1:clx], col=1:clx);	
+	}
+	maxmsd=max(msd_mat_b, na.rm=T);
+	plot(0,0, type="n", xlim=c(1, num_cuts), ylim=c(0, maxmsd), xlab="Cluster Cuts", ylab="MSD", main=paste(nameb, " clustered by ", namea, sep=""));
+	abline(h=msd_mat_b[1,1], col="blue", lty=2);
+	for(clx in 1:num_cuts){
+		points(rep(clx, clx), msd_mat_b[clx, 1:clx], col=1:clx);	
+	}
+
 }
 
-orig_dendr=as.dendrogram(hcl);
 
-# Extract names of leaves from left to right
-lf_names=get_clstrd_leaf_names(orig_dendr);
-
-pdf(paste(output_fname_root, ".ch_stop.pdf", sep=""), height=8.5, width=14);
-
-palette(palette_col);
-
-# Compute ISO and classical MDS
-nonparm_mds_res=matrix(0,nrow=num_samples,ncol=2);
-classic_mds_res=matrix(0,nrow=num_samples,ncol=2);
-
-cat("Computing classic and non parametric MDS...\n");
-imds_res=isoMDS(full_dist_mat);
-nonparm_mds_res[,1]=imds_res$points[,1]
-nonparm_mds_res[,2]=imds_res$points[,2]
-classic_mds_res=cmdscale(full_dist_mat);
-
-###############################################################################
-
-denfun.label_scale=min(2,55/num_samples);
-
-###############################################################################
+analyze_subclusters(hcl_A, hcl_B, dist_mat_A, dist_mat_B, cuts, map_info[["a"]],  map_info[["b"]], map_info[["b_id"]]);
+analyze_subclusters(hcl_B, hcl_A, dist_mat_B, dist_mat_A, cuts, map_info[["b"]],  map_info[["a"]], map_info[["a_id"]]);
 
 
-###############################################################################
+##############################################################################
 
 cat("\nDone.\n")
 dev.off();
