448d2f3b135d9924782b147876a98bda18d08a1a
Li <lik2@upmc.edu>
2020-10-22 13:47:00 -0400

    added export of simplified ANOVA table

--- Profile/abundance_based/Fit_ALR_as_Response/Fit_ALR_as_Response.r
+++ Profile/abundance_based/Fit_ALR_as_Response/Fit_ALR_as_Response.r
@@ -1586,6 +1586,23 @@ uv_coeff_fname=paste(OutputRoot, ".uv_coeff.tsv", sep="");
 write.table(t(uv_coeff_mat), file=uv_coeff_fname, quote=F, sep="\t", col.names=NA, row.names=T);
 
 ##############################################################################
+# Write MANOVA pvalues to file
+
+if(length(manova_res)){
+        num_variables=nrow(manova_res)-1;
+        outmat=matrix("", nrow=num_variables, ncol=2);
+        colnames(outmat)=c(TagName, "Pr(>F)");
+        varnames=unlist(rownames(manova_res));
+        pvals=unlist(manova_res["Pr(>F)"]);
+        outmat[,TagName]=varnames[1:num_variables];
+        outmat[,"Pr(>F)"]=sprintf("%4.4f", pvals[1:num_variables]);
+}else{
+        outmat=matrix("-", nrow=1, ncol=2);
+        colnames(outmat)=c(TagName, "Pr(>F)");
+}
+write.table(outmat, file=paste(OutputRoot, ".anova.summary.tsv", sep=""), sep="\t", quote=F, col.names=T, row.names=F);
+
+##############################################################################
 
 cat("Done.\n");
 #dev.off();
