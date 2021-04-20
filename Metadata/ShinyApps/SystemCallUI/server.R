80c2f827e609da027bc16f2a82e9e7e16710db58
kelvin2016 <kelvin2016@users.noreply.github.com>
2021-04-19 23:13:39 -0400

    updates to UI better feedback

--- Metadata/ShinyApps/SystemCallUI/server.R
+++ Metadata/ShinyApps/SystemCallUI/server.R
@@ -1,69 +1,238 @@
-library(shiny)
+library(shiny);
+library(shinyjs);
+
+setRunReady=function(input, output, session){
+	
+	select_arr=files_selected(input, output, session);
+	
+	if(any(select_arr)){
+		output$runNotReadyTxt=renderText({""});
+		output$runReadyTxt=renderText({"Ready"});
+	}else{
+		output$runNotReadyTxt=renderText({"Not Ready"});
+		output$runReadyTxt=renderText({""});	
+	}
+}
+
+observeCheckboxEvents=function(input, output, session){
+	observeEvent(input$file1use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file1use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});
+	observeEvent(input$file2use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file2use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});	
+	observeEvent(input$file3use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file3use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});	
+	observeEvent(input$file4use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file4use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});	
+	observeEvent(input$file5use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file5use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});	
+	observeEvent(input$file6use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file6use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});	
+	observeEvent(input$file7use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file7use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});
+	observeEvent(input$file8use_cb, {
+		if(ntb(input$file1_upld$datapath)==""){
+			updateCheckboxInput(session, "file8use_cb", value=F);
+		}
+		setRunReady(input, output, session);
+	});
+}
+
+observeFileUploadsEvents=function(input, output, session){
+
+	observeEvent(input$file1_upld, {
+		updateCheckboxInput(session, "file1use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+
+	observeEvent(input$file2_upld, {
+		updateCheckboxInput(session, "file2use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+
+	observeEvent(input$file3_upld, {
+		updateCheckboxInput(session, "file3use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+
+	observeEvent(input$file4_upld, {
+		updateCheckboxInput(session, "file4use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+
+	observeEvent(input$file5_upld, {
+		updateCheckboxInput(session, "file5use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+
+	observeEvent(input$file6_upld, {
+		updateCheckboxInput(session, "file6use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+
+	observeEvent(input$file7_upld, {
+		updateCheckboxInput(session, "file7use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+
+	observeEvent(input$file8_upld, {
+		updateCheckboxInput(session, "file8use_cb", value=T);
+		setRunReady(input, output, session);
+		});
+		
+}
+
+ntb=function(x){
+	if(is.null(x)){
+		return("");
+	}else{
+		return(x);
+	}
+}
+
+files_selected=function(input, output, session){
+
+	file_cb=c(
+		input$file1use_cb,
+		input$file2use_cb,
+		input$file3use_cb,
+		input$file4use_cb,
+		input$file5use_cb,
+		input$file6use_cb,
+		input$file7use_cb,
+		input$file8use_cb);
+	
+	return(file_cb);
+}
+
+files_specified=function(input, output, session){
+
+	file_path=c(
+		ntb(input$file1_upld$datapath),
+		ntb(input$file2_upld$datapath),
+		ntb(input$file3_upld$datapath),
+		ntb(input$file4_upld$datapath),
+		ntb(input$file5_upld$datapath),
+		ntb(input$file6_upld$datapath),
+		ntb(input$file7_upld$datapath),
+		ntb(input$file8_upld$datapath)
+	);
+	
+	return(file_path);
+}
 
 function(input, output, session) {
+	
+	output$downloadNotReadyTxt=renderText({"Not Ready"});
+	output$runNotReadyTxt=renderText({"Not Ready"});
+	
+	output$downloadReadyTxt=renderText({""});
+	output$runReadyTxt=renderText({""});
+	
+	output$appstdoutReadyTxt=renderText({"(Nothing yet...)"});
+	
+	observeFileUploadsEvents(input, output, session);
+	observeCheckboxEvents(input, output, session);
+
+	observeEvent(input$runButton, {
+		# This is where the uploaded file is on the server
+		filelist=files_specified(input, output, session);
+			
+		# This is the name of the file the user uploaded (w/o the full path)
+		filenames=c(
+			ntb(input$file1_upld$name),
+			ntb(input$file2_upld$name),
+			ntb(input$file3_upld$name),
+			ntb(input$file4_upld$name),
+			ntb(input$file5_upld$name),
+			ntb(input$file6_upld$name),
+			ntb(input$file7_upld$name),
+			ntb(input$file8_upld$name)
+		);
+		
+		# This is where the uploaded file is on the server
+		fileuse=files_selected(input, output, session);
+		
+		# Select out the files that can be used
+		isNotEmpty=ifelse(filelist!="", T, F);
+		filelist=filelist[fileuse&isNotEmpty];
+		filenames=filenames[fileuse&isNotEmpty];
+		
+		cat("User File Names:\n");
+		print(filenames);
+		
+		cat("Uploaded File Paths:\n");
+		print(filelist);
+		
+		# Write all the files user uploaded, into a table:  1st col "path on server", 2nd col "user file name"
+		file_of_paths=tempfile();
+		write.table(cbind(filelist, filenames), file_of_paths, sep="\t", row.names=F, col.names=F, quote=F);
+		
+		# Get path of where we are allowed to write a temp file
+		output_root=tempfile();
+		
+		# Generate system command and then run it.
+		mycmd=paste("Rscript www/AccumulateQCAcrossRuns.r -i ", file_of_paths, " -o ", output_root, sep="");
+		mydata=system(mycmd, intern=T);
+		
+		# Debugging output from STDOUT
+		print(mydata);
+		output$appstdoutReadyTxt=renderText({paste(mydata, collapse="\n"); })
+		
+		output$downloadNotReadyTxt=renderText({""});
+		output$downloadReadyTxt=renderText({"Ready"});
+		
+		session$userData[["output_files"]]=c(
+			first=paste(output_root, ".accumulated.detail.tsv", sep=""),
+			second=paste(output_root, ".accumulated.simplify.tsv", sep="")
+		);
+		
+	});
+	
 	
 	output$downloadButton=downloadHandler(
 		filename=function(){
 			paste("Results.zip");
 		},
 		content=function(filename){
-			
 			cat("Write to this file:\n");
 			print(filename);
 			
-			# This is where the uploaded file is on the server
-			filelist=c(
-				input$file1$datapath,
-				input$file2$datapath,
-				input$file3$datapath,
-				input$file4$datapath,
-				input$file5$datapath,
-				input$file6$datapath,
-				input$file7$datapath,
-				input$file8$datapath);
-				
-			# This is the name of the file the user uploaded (w/o the full path)
-			filenames=c(
-				input$file1$name,
-				input$file2$name,
-				input$file3$name,
-				input$file4$name,
-				input$file5$name,
-				input$file6$name,
-				input$file7$name,
-				input$file8$name
-			)
-			
-			cat("User File Names:\n");
-			print(filenames);
-			
-			cat("Uploaded File Paths:\n");
-			print(filelist);
-			
-			# Write all the files user uploaded, into a table:  1st col "path on server", 2nd col "user file name"
-			file_of_paths=tempfile();
-			write.table(cbind(filelist, filenames), file_of_paths, sep="\t", row.names=F, col.names=F, quote=F);
-			
-			# Get path of where we are allowed to write a temp file
-			output_root=tempfile();
-			
-			# Generate system command and then run it.
-			mycmd=paste("Rscript www/AccumulateQCAcrossRuns.r -i ", file_of_paths, " -o ", output_root, sep="");
-			mydata=system(mycmd, intern=T);
-			
-			# Debugging output from STDOUT
-			print(mydata);
-			
-			# Generate a list of files 
-			first=paste(output_root, ".accumulated.detail.tsv", sep="");
-			second=paste(output_root, ".accumulated.simplify.tsv", sep="");
-			
 			# Zip file together and place results into the filename specified by shiny
-			cat("File name to write our output:\n");
-			print(c(first,second));
-			zip(zipfile=filename, files=c(first, second));
+			cat("Zipping these files:\n");
+			print(session$userData[["output_files"]]);
+			
+			zip(zipfile=filename, files=session$userData[["output_files"]]);
 			cat("Done zipping.\n");
 		}
 	);
+	
 
 }
--- Metadata/ShinyApps/SystemCallUI/ui.R
+++ Metadata/ShinyApps/SystemCallUI/ui.R
@@ -6,42 +6,69 @@ fluidPage(
 
   sidebarPanel(
 
-	fileInput("file1", "Choose 1st TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv")),
-	fileInput("file2", "Choose 2nd TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv")),
-	fileInput("file3", "Choose 3rd TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv")),
-	fileInput("file4", "Choose 4th TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv")),
-	fileInput("file5", "Choose 5th TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv")),
-	fileInput("file6", "Choose 6th TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv")),
-	fileInput("file7", "Choose 7th TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv")),
-	fileInput("file8", "Choose 8th TSV File",
-                multiple = FALSE,
-                accept = c("text/tsv",
-                         ".tsv"))
+	tags$h5(tags$b("Select your input files:")),
+	tags$h6(tags$p("Use the checkbox to disable an uploaded file.")),
+	tags$h6(tags$p("You may replaced a previously uploaded file by selecting a new file to replace it.")), 
+
+	fluidRow(
+		column(1, checkboxInput("file1use_cb", "", value=F)),
+		column(8, fileInput("file1_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
+
+	fluidRow(
+		column(1, checkboxInput("file2use_cb", "", value=F)),
+		column(8, fileInput("file2_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
+
+	fluidRow(
+		column(1, checkboxInput("file3use_cb", "", value=F)),
+		column(8, fileInput("file3_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
+
+	fluidRow(
+		column(1, checkboxInput("file4use_cb", "", value=F)),
+		column(8, fileInput("file4_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
+
+	fluidRow(
+		column(1, checkboxInput("file5use_cb", "", value=F)),
+		column(8, fileInput("file5_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
+
+	fluidRow(
+		column(1, checkboxInput("file6use_cb", "", value=F)),
+		column(8, fileInput("file6_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
+
+	fluidRow(
+		column(1, checkboxInput("file7use_cb", "", value=F)),
+		column(8, fileInput("file7_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
+
+	fluidRow(
+		column(1, checkboxInput("file8use_cb", "", value=F)),
+		column(8, fileInput("file8_upld", NULL, multiple = FALSE, accept = c("text/tsv", ".tsv")))
+		),
   ),
 
   mainPanel(
+	tags$p("When you are ready to run, press this button:"),
+	fluidRow(
+		column(2, actionButton("runButton", "Run")),
+		column(1, span(textOutput("runNotReadyTxt"), style="color:red")),
+		column(1, span(textOutput("runReadyTxt"), style="color:green"))
+	),
+	tags$p(HTML("&nbsp")),
+	
 	tags$p("When the tool has completed running, you may download the output:"),
-	downloadButton("downloadButton", "Download")
+	fluidRow(
+		column(2, downloadButton("downloadButton", "Download")),
+		column(1, span(textOutput("downloadNotReadyTxt"), style="color:red")),
+		column(1, span(textOutput("downloadReadyTxt"), style="color:green"))
+	),
+	tags$p(HTML("&nbsp")),
+	
+	tags$p("Standard Output from Running Tool:"),
+	verbatimTextOutput("appstdoutReadyTxt", placeholder=T)
   )
 )
