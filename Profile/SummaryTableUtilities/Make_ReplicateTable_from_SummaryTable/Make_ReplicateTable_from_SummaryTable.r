561975438e794c4c823ef7c270c6587853dc6f6f
Li <lik2@upmc.edu>
2021-09-29 16:26:59 -0400

    first check in

---
+++ Profile/SummaryTableUtilities/Make_ReplicateTable_from_SummaryTable/Make_ReplicateTable_from_SummaryTable.r
@@ -0,0 +1,109 @@
+#!/usr/bin/env Rscript
+
+###############################################################################
+
+library('getopt');
+
+params=c(
+	"input_summary_table", "s", 1, "character",
+	"output_mapping_file", "m", 2, "character"
+);
+
+opt=getopt(spec=matrix(params, ncol=4, byrow=TRUE), debug=FALSE);
+
+script_name=unlist(strsplit(commandArgs(FALSE)[4],"=")[1])[2];
+
+usage = paste (
+	"\nUsage:\n\n", script_name,
+	"\n",
+	"	-s <input .summary_table.tsv>\n",
+	"	-m <output mapping file, tab-separated>\n",
+	"\n",	
+	"This script will generate a mapping table based on the\n",
+	"input summary table's sample IDs.  The first column\n",
+	"is the original sample ID with replicate extension, the\n",
+	"the second column will contain the name of the collapsed or\n",
+	"underlying sample ID.\n",
+	"\n",
+	"For example:\n",
+	"0208.004012.20180910.BALR	0208.004012.20180910.BALR\n",
+	"0208.004012.20180910.BALR.2	0208.004012.20180910.BALR\n",
+	"\n",
+	"Mapping File format:\n",
+	"	<current sample id>\\t<new sample id>\\n\n",
+	"\n");
+
+if(!length(opt$input_summary_table) || !length(opt$output_mapping_file)){
+	cat(usage);
+	q(status=-1);
+}
+
+InputSummaryTable=opt$input_summary_table;
+OuputMappingFile=opt$output_mapping_file;
+
+###############################################################################
+
+cat("\n")
+cat("Input Summary Table Name: ", InputSummaryTable, "\n");
+cat("Output Mapping File Name: ", OuputMappingFile, "\n");       
+cat("\n");
+
+###############################################################################
+###############################################################################
+
+# Load data
+cat("Loading Matrix...\n");
+inmat=as.matrix(read.table(InputSummaryTable, sep="\t", header=TRUE, 
+	check.names=FALSE, comment.char="*", row.names=1))
+
+#cat("Input Excerpt:\n")
+#print(inmat[1:10,1:10]);
+
+###############################################################################
+
+sample_ids=rownames(inmat);
+
+cat("Sample ID Excerpt: \n");
+print(sample_ids[1:10]);
+
+num_samp_ids=length(sample_ids);
+cat("Number of Sample IDs: ", num_samp_ids, "\n");
+
+collapsed_id=character(num_samp_ids);
+
+
+cat("\n");
+for(i in 1:num_samp_ids){
+
+	cur_samp_id=sample_ids[i];
+
+	toks=strsplit(cur_samp_id, "\\.")[[1]];
+	num_toks=length(toks);
+	if(!is.na(as.numeric(toks[num_toks]))){
+		toks=toks[-num_toks];
+	}
+
+	collapsed_id[i]=paste(toks, collapse=".");
+}
+
+
+###############################################################################
+# Output
+cat("Writing Mapping File...\n");
+fc=file(OuputMappingFile, "w");
+
+cat(file=fc, "Original\tCollapsed\n");
+for(samp_idx in 1:num_samp_ids){
+	cat(file=fc, sample_ids[samp_idx], "\t", collapsed_id[i], "\n", sep="");
+}
+close(fc);	
+
+###############################################################################
+
+writeLines("Done.\n")
+warns=warnings();
+if(!is.null(warns)){
+	print(warns);
+}
+
+q(status=0)
---
+++ Profile/SummaryTableUtilities/Make_ReplicateTable_from_SummaryTable/example/run.csh
@@ -0,0 +1,5 @@
+#!/bin/csh
+
+../Make_ReplicateTable_from_SummaryTable.r \
+	-s example.summary_table.tsv \
+	-m example.map.tsv
