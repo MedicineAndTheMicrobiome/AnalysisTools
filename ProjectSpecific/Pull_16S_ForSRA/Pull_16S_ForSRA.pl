2abc0d8c4814f7cc3e9d33b6821ff0739ac95a73
Li <lik2@upmc.edu>
2022-07-29 14:03:28 -0400

    added extra columns

--- ProjectSpecific/Pull_16S_ForSRA/Pull_16S_ForSRA.pl
+++ ProjectSpecific/Pull_16S_ForSRA/Pull_16S_ForSRA.pl
@@ -22,6 +22,10 @@ $0
 	-p <'list of project IDs to include', 0000 and 0001, negative controls automatically included>
 	-o <Output directory>
 
+	Optional:
+	[-i <include list, exclude other samples found>]
+	[-t <build tgz file of fastq files, otherwise just create directory of symbolic links>]
+
 	This script will look into the SequencingRuns directory, finding the sequencing runs
 	specified in the sequencing runs list.  For each sequencing run, it will look into
 	
@@ -222,20 +226,29 @@ $local_scale_hash{"OralWash"}="mouth [UBERON:0001165]";
 $local_scale_hash{"Saliva"}="mouth [UBERON:0001165]";
 
 my %control_hash;
-$control_hash{"0000"}="Extraction_Kit_Negative_Control#kit_reagents";
-$control_hash{"0001"}="PCR_Amplification_Negative_Control#pcr_reagents";
-$control_hash{"0002"}="PCR_Amplification_PositiveZymo_Control#control_DNA";
+$control_hash{"0000"}="Extraction_Kit_Negative_Control#kit_reagents#metagenome";
+$control_hash{"0001"}="PCR_Amplification_Negative_Control#pcr_reagents#metagenome";
+$control_hash{"0002"}="PCR_Amplification_PositiveZymo_Control#control_DNA#synthetic metagenome";
 
+my %organism_hash;
+$organism_hash{"Human"}="Homo sapiens";
+$organism_hash{"Rat"}="Rattus norvegicus";
+$organism_hash{"Mouse"}="Mus musculus";
+
+my $env_broad_scale_default="multicellular organism [UBERON:0000468]";
 
 my @header=(
 	"sample_name",
 	"library_id",
 	"title",
+	"organism",
 	"collection_date", 
+	"env_broad_scale",
 	"env_local_scale",
 	"env_medium",
-	"host1",
-	"host2",
+	"host",
+	"host_subject_id1",
+	"host_subject_id2",
 	"filename",
 	"filename2"
 );
@@ -263,26 +276,33 @@ for(my $i=0; $i<$num_entries; $i++){
 	my $sample_date_form="$sample_year-$sample_mon-$sample_day";
 
 	# Parse sample types
-	my $sample_type=$sample_type_hash{$sample_name_toks[4]};
-	my @sample_type_tok=split "#", $sample_type;
+	my $sample_type_rec=$sample_type_hash{$sample_name_toks[4]};
+	my @sample_type_tok=split "#", $sample_type_rec;
 	my $tissue=$sample_type_tok[0];
-	my $host=$sample_type_tok[1];
-	my $medium=$medium_hash{$tissue};
-	my $local_scale=$local_scale_hash{$tissue};
+	my $env_medium=$medium_hash{$tissue};
+	my $env_local_scale=$local_scale_hash{$tissue};
+	my $env_broad_scale=$env_broad_scale_default;
 	my $title="16S_Microbiome_" . $tissue;
+	my $host=$organism_hash{$sample_type_tok[1]};
+	my $organism=lc("$sample_type_tok[1] $tissue microbiome");
 
 	my $host_subject_id1=$sample_name_toks[1];
 	my $host_subject_id2=$sample_name_toks[2];
 
 	my $library_id="$run_list[$i]\.$sample_name";
 
+	# Override variable values if it is a control
 	my $control_info=$control_hash{$sample_name_toks[0]};
 	if(defined($control_info)){
-		my ($control_name, $control_scale)=split "#", $control_info;
+		my ($control_name, $control_scale, $control_organism)=split "#", $control_info;
 		$sample_name=$control_name;
-		$local_scale=$control_scale;
-		$medium=$control_scale;
+		$env_local_scale=$control_scale;
+		$env_broad_scale=$control_scale;
+		$env_medium=$control_scale;
+		$organism=$control_organism;
+		$host=$control_scale;
 		
+		# Reformat control processing date
 		$sample_date_raw=$sample_name_toks[2];
 		$sample_year=substr($sample_date_raw, 0, 4);
 		$sample_mon=substr($sample_date_raw, 4, 2);
@@ -291,7 +311,21 @@ for(my $i=0; $i<$num_entries; $i++){
 	}
 
 	# Generate output string
-	my $outstr=join "\t", ($sample_name, $library_id, $title, $sample_date_form, $local_scale, $medium, $host_subject_id1, $host_subject_id2, $f1_list[$i], $f2_list[$i]);
+	my $outstr=join "\t", (
+		$sample_name, 
+		$library_id, 
+		$title, 
+		$organism,
+		$sample_date_form, 
+		$env_broad_scale,
+		$env_local_scale, 
+		$env_medium, 
+		$host,	#scientific name
+		$host_subject_id1, 
+		$host_subject_id2, 
+		$f1_list[$i], 
+		$f2_list[$i]
+	);
 
 	print FH "$outstr\n";
 	
