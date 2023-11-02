da3e0ac72febfdbfcd1aaa42527291d8488292be
Li <lik2@upmc.edu>
2023-11-02 12:20:08 -0400

    fixed err when not enough degrees of freedom

--- Profile/distribution_based/Fit_TwoSample_Diversity_Regression/Fit_PairedDiff_Diversity_Regression.r
+++ Profile/distribution_based/Fit_TwoSample_Diversity_Regression/Fit_PairedDiff_Diversity_Regression.r
@@ -488,7 +488,7 @@ signf_as_table=function(coef_mat, pval_mat){
                 for(j in 1:num_cols){
 
                         pval=pval_mat[i,j];
-                        if(pval < 0.10){
+                        if(!is.na(pval) && !is.nan(pval) && pval < 0.10){
                                 comb_tab[line_ix, "Row"]=rnames[i];
                                 comb_tab[line_ix, "Column"]=cnames[j];
                                 comb_tab[line_ix, "Coefficient"]=sprintf("%.5g", coef_mat[i,j]);
@@ -831,19 +831,25 @@ for(div_ix in 1:num_div_idx){
 
 	cat("\n\n*************************************************\n\n");
 
+
 }
 
 ###############################################################################
 
-all.nas=apply(covariates_coef_mat, 2, function(x){all(is.na(x))});
+all.nas=apply(covariates_coef_mat, 2, function(x){all(is.na(x) || is.nan(x))});
 covariates_coef_mat=covariates_coef_mat[,!all.nas,drop=F];
 
-all.nas=apply(covariates_pval_mat, 2, function(x){all(is.na(x))});
+# If covariates can be estimated, go nan for pvalues
+#all.nas=apply(covariates_pval_mat, 2, function(x){all(is.na(x) || is.nan(x))});
 covariates_pval_mat=covariates_pval_mat[,!all.nas,drop=F];
 
+cat("Covariates Coef:\n");
 print(covariates_coef_mat);
+cat("Covariates Pval:\n"); 
 print(covariates_pval_mat);
-print(rsqrd_mat);
+cat("Rsqrd:\n");
+print(rsqrd_mat)
+cat("Model Pval:\n");
 print(model_pval_mat);
 
 par(oma=c(2,1,5,2));
