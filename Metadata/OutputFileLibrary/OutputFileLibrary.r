ad255788b766d693585f7eff39f8ddb52eb458d1
Li <lik2@upmc.edu>
2024-03-16 00:02:40 -0400

    first checkin

---
+++ Metadata/OutputFileLibrary/OutputFileLibrary.r
@@ -0,0 +1,379 @@
+
+##############################################################################
+
+plot_text=function(strings, echo=F){
+	par(family="Courier");
+	par(oma=rep(1,4));
+	par(mar=rep(0,4));
+
+	num_lines=length(strings);
+	
+	top=max(as.integer(num_lines), 52);
+
+	plot(0,0, xlim=c(0,top), ylim=c(0,top), type="n",  xaxt="n", yaxt="n",
+		xlab="", ylab="", bty="n", oma=c(1,1,1,1), mar=c(0,0,0,0)
+		);
+
+	text_size=max(.01, min(.8, .8 - .003*(num_lines-52)));
+	#print(text_size);
+
+	for(i in 1:num_lines){
+		#cat(strings[i], "\n", sep="");
+		strings[i]=gsub("\t", "", strings[i]);
+		text(0, top-i, strings[i], pos=4, cex=text_size); 
+
+		if(echo){
+			cat(strings[i],"\n");
+		}
+	}
+}
+
+plot_title_page=function(title, subtitle=""){
+
+        orig.par=par(no.readonly=T);
+        par(family="serif");
+        par(mfrow=c(1,1));
+
+        plot(0,0, xlim=c(0,1), ylim=c(0,1), type="n",  xaxt="n", yaxt="n",
+                xlab="", ylab="", bty="n", oma=c(1,1,1,1), mar=c(0,0,0,0)
+                );
+
+        # Title
+        title_cex=3;
+        title_line=1;
+        text(0.5, title_line, title, cex=title_cex, font=2, adj=c(.5,1));
+
+        # Subtitle
+        num_subt_lines=length(subtitle);
+        cxy=par()$cxy;
+        for(i in 1:num_subt_lines){
+                text(.5, title_line -title_cex*cxy[2] -i*cxy[2], subtitle[i], adj=.5);
+        }
+
+        par(orig.par);
+}
+
+
+paint_matrix=function(mat, title="", plot_min=NA, plot_max=NA, log_col=F, high_is_hot=T, deci_pts=4, 
+	label_zeros=T, counts=F, value.cex=2, 
+	plot_col_dendr=F,
+	plot_row_dendr=F
+){
+
+        num_row=nrow(mat);
+        num_col=ncol(mat);
+
+	row_names=rownames(mat);
+	col_names=colnames(mat);
+
+	orig.par=par(no.readonly=T);
+
+	cat("Painting Matrix: ", title, "\n");
+        cat("Num Rows: ", num_row, "\n");
+        cat("Num Cols: ", num_col, "\n");
+
+
+	if(num_row==0 || num_col==0){
+		plot(0, type="n", xlim=c(-1,1), ylim=c(-1,1), xaxt="n", yaxt="n", bty="n", xlab="", ylab="",
+			main=title);
+		text(0,0, "No data to plot...");
+		return();
+	}
+
+	# Flips the rows, so becuase origin is bottom left
+        mat=mat[rev(1:num_row),, drop=F];
+
+	# Generate a column scheme
+        num_colors=50;
+        color_arr=rainbow(num_colors, start=0, end=4/6);
+        if(high_is_hot){
+                color_arr=rev(color_arr);
+        }
+
+	# Provide a means to map values to an (color) index 
+        remap=function(in_val, in_range, out_range){
+                in_prop=(in_val-in_range[1])/(in_range[2]-in_range[1])
+                out_val=in_prop*(out_range[2]-out_range[1])+out_range[1];
+                return(out_val);
+        }
+
+	# If range is not specified, find it based on the data
+        if(is.na(plot_min)){
+                plot_min=min(mat);
+        }
+        if(is.na(plot_max)){
+                plot_max=max(mat);
+        }
+        cat("Plot min/max: ", plot_min, "/", plot_max, "\n");
+
+	# Get Label lengths
+	row_max_nchar=max(nchar(row_names));
+	col_max_nchar=max(nchar(col_names));
+	cat("Max Row Names Length: ", row_max_nchar, "\n");
+	cat("Max Col Names Length: ", col_max_nchar, "\n");
+
+	##################################################################################################
+	
+	get_dendrogram=function(in_mat, type){
+		if(type=="row"){
+			dendist=dist(in_mat);
+		}else{
+			dendist=dist(t(in_mat));
+		}
+		
+		get_clstrd_leaf_names=function(den){
+		# Get a list of the leaf names, from left to right
+			den_info=attributes(den);
+			if(!is.null(den_info$leaf) && den_info$leaf==T){
+				return(den_info$label);
+			}else{
+				lf_names=character();
+				for(i in 1:2){
+					lf_names=c(lf_names, get_clstrd_leaf_names(den[[i]]));
+				}
+				return(lf_names);
+			}
+		}
+
+		hcl=hclust(dendist, method="ward.D2");
+		dend=list();
+		dend[["tree"]]=as.dendrogram(hcl);
+		dend[["names"]]=get_clstrd_leaf_names(dend[["tree"]]);
+		return(dend);
+	}
+
+
+	##################################################################################################
+	# Comput Layouts
+	col_dend_height=ceiling(num_row*.1);
+	row_dend_width=ceiling(num_col*.2);
+	
+	heatmap_height=num_row;
+	heatmap_width=num_col;
+
+	if(num_row==1){
+		plot_row_dendr=F;
+	}
+	if(num_col==1){
+		plot_col_dendr=F;
+	}
+
+	# Don't plot dendrogram if there are any NAs in the matrix
+	if(any(is.na(mat))){
+		plot_col_dendr=F;
+		plot_row_dendr=F;
+	}
+
+	if(plot_col_dendr && plot_row_dendr){
+		layoutmat=matrix(
+			c(
+			rep(c(rep(4, row_dend_width), rep(3, heatmap_width)), col_dend_height),
+			rep(c(rep(2, row_dend_width), rep(1, heatmap_width)), heatmap_height)
+			), byrow=T, ncol=row_dend_width+heatmap_width);
+
+		col_dendr=get_dendrogram(mat, type="col");
+		row_dendr=get_dendrogram(mat, type="row");
+
+		mat=mat[row_dendr[["names"]], col_dendr[["names"]], drop=F];
+		
+	}else if(plot_col_dendr){
+		layoutmat=matrix(
+			c(
+			rep(rep(2, heatmap_width), col_dend_height),
+			rep(rep(1, heatmap_width), heatmap_height)
+			), byrow=T, ncol=heatmap_width); 
+
+		col_dendr=get_dendrogram(mat, type="col");
+		mat=mat[, col_dendr[["names"]], drop=F];
+		
+	}else if(plot_row_dendr){
+		layoutmat=matrix(
+			rep(c(rep(2, row_dend_width), rep(1, heatmap_width)), heatmap_height),
+			byrow=T, ncol=row_dend_width+heatmap_width);
+
+		row_dendr=get_dendrogram(mat, type="row");
+		mat=mat[row_dendr[["names"]],,drop=F];
+	}else{
+		layoutmat=matrix(
+			rep(1, heatmap_height*heatmap_width), 
+			byrow=T, ncol=heatmap_width);
+	}
+
+	#print(layoutmat);
+	layout(layoutmat);
+
+	##################################################################################################
+	
+	par(oma=c(col_max_nchar*.60, 0, 3, row_max_nchar*.60));
+	par(mar=c(0,0,0,0));
+        plot(0, type="n", xlim=c(0,num_col), ylim=c(0,num_row), xaxt="n", yaxt="n", bty="n", xlab="", ylab="");
+	mtext(title, side=3, line=0, outer=T, font=2);
+
+        # x-axis
+        axis(side=1, at=seq(.5, num_col-.5, 1), labels=colnames(mat), las=2, line=-1.75);
+        axis(side=4, at=seq(.5, num_row-.5, 1), labels=rownames(mat), las=2, line=-1.75);
+
+        if(log_col){
+                plot_min=log10(plot_min+.0125);
+                plot_max=log10(plot_max+.0125);
+        }
+
+        for(x in 1:num_col){
+                for(y in 1:num_row){
+
+                        if(log_col){
+                                col_val=log10(mat[y,x]+.0125);
+                        }else{
+                                col_val=mat[y,x];
+                        }
+
+                        remap_val=remap(col_val, c(plot_min, plot_max), c(1, num_colors));
+                        col_ix=ceiling(remap_val);
+
+                        rect(x-1, y-1, (x-1)+1, (y-1)+1, border=NA, col=color_arr[col_ix]);
+
+                        if(mat[y,x]!=0 || label_zeros){
+                                if(counts){
+                                        text_lab=sprintf("%i", mat[y,x]);
+                                }else{
+                                        text_lab=sprintf(paste("%0.", deci_pts, "f", sep=""), mat[y,x]);
+                                }
+                                text(x-.5, y-.5, text_lab, srt=atan(num_col/num_row)/pi*180, 
+					cex=value.cex, font=2);
+                        }
+                }
+        }
+
+	##################################################################################################
+
+	par(mar=c(0, 0, 0, 0));
+
+	if(plot_row_dendr && plot_col_dendr){
+		rdh=attributes(row_dendr[["tree"]])$height;
+		cdh=attributes(col_dendr[["tree"]])$height;
+		plot(row_dendr[["tree"]], leaflab="none", horiz=T, xaxt="n", yaxt="n", 
+			bty="n", xlim=c(rdh, 0));
+		plot(col_dendr[["tree"]], leaflab="none",xaxt="n", yaxt="n", bty="n", ylim=c(0, cdh));
+		plot(0,0, type="n", bty="n", xaxt="n", yaxt="n");
+		#text(0,0, "Placeholder");
+	}else if(plot_row_dendr){
+		rdh=attributes(row_dendr[["tree"]])$height;
+		plot(row_dendr[["tree"]], leaflab="none", horiz=T, xaxt="n", yaxt="n", 
+			bty="n", xlim=c(rdh, 0));
+		#text(0,0, "Row Dendrogram");
+	}else if(plot_col_dendr){
+		cdh=attributes(col_dendr[["tree"]])$height;
+		plot(col_dendr[["tree"]], leaflab="none", xaxt="n", yaxt="n", bty="n", ylim=c(0, cdh));
+		#text(0,0, "Col Dendrogram");
+	}
+
+	par(orig.par);
+
+}
+
+add_sign_col=function(coeff){
+
+	if(nrow(coeff)==0){
+		return(coeff);
+	}else{
+
+		cnames=colnames(coeff);
+		pval_ix=which(cnames=="Pr(>|t|)");
+		pval=coeff[,pval_ix];
+
+		sig_char=function(val){
+			if(!is.null(val) && !is.nan(val) && !is.na(val)){
+				if(val <= .0001){ return("***");}
+				if(val <= .001 ){ return("** ");}
+				if(val <= .01  ){ return("*  ");}
+				if(val <= .05  ){ return(":  ");}
+				if(val <= .1   ){ return(".  ");}
+				return(" ");
+			}else{
+				return(" ");
+			}
+		}
+
+		sig_arr=sapply(pval, sig_char);
+		fdr=round(p.adjust(pval, method="fdr"), 4);
+		fdr_sig_char=sapply(fdr, sig_char);
+		#out_mat=cbind(coeff, fdr);
+
+		fmt=function(x){
+			return(formatC(x, format="f", digits=4, width=7));
+		}
+
+		out_mat=cbind(
+			fmt(coeff), sig_arr, fmt(fdr), fdr_sig_char);
+		colnames(out_mat)=c(cnames, "Signf", "FDR", "Signf");
+		
+		return(out_mat);
+	}	
+}
+
+signf_char=function(val){
+	if(!is.null(val) && !is.nan(val) && !is.na(val)){
+		if(val <= .0001){ return("***");}
+		if(val <= .001 ){ return("** ");}
+		if(val <= .01  ){ return("*  ");}
+		if(val <= .05  ){ return(":  ");}
+		if(val <= .1   ){ return(".  ");}
+	}
+	return(" ");
+}
+
+###############################################################################
+
+mask_matrix=function(val_mat, mask_mat, mask_thres, mask_val){
+        masked_matrix=val_mat;
+        masked_matrix[mask_mat>mask_thres]=mask_val;
+        return(masked_matrix);
+}
+
+###############################################################################
+
+plot_histograms=function(data_mat, title="", max_col_plots=3, max_row_plots=4){
+	
+        orig.par=par(no.readonly=T);
+
+	num_var=ncol(data_mat);
+	var_names=colnames(data_mat);
+
+	par(oma=c(1,0,2,0));
+	par(mar=c(3,4,5,1));
+
+	# Calculate num plots per row/col
+	sqrt_num_var=ceiling(sqrt(num_var));
+	num_col_plots=min(max_col_plots, sqrt_num_var);
+	num_row_plots=min(max_row_plots, sqrt_num_var);
+	ppp=num_col_plots*num_row_plots
+	par(mfrow=c(num_row_plots, num_col_plots));
+	
+	# Generate plots
+	for(i in 1:num_var){
+
+		data=data_mat[,i]
+		hist(data, main="", xlab="", ylab="Counts");
+	
+		# Calculate quick descriptive stats
+		mean=mean(data);
+		qtl=quantile(data, c(0.025,0.5,0.975));
+		rng=range(data);
+
+		# Label quick stats
+		title(main=var_names[i], cex.main=1.5, font.main=2);
+		title(main=sprintf("mean = %.3g, median = %.3g", 
+			mean, qtl[2]), line=-.5, cex.main=.9, font.main=3);
+		title(main=sprintf("95%% CI (LB = %.3g, UB = %.3g)", 
+			qtl[1], qtl[3]), line=-1.4, cex.main=.9, font.main=3);
+		title(main=sprintf("min = %.3g, max = %.3g", rng[1], rng[2]), 
+			line=-2.3, cex.main=.9, font.main=3);
+
+		# If first plot on page (new page), then label the outer margins
+		if(((i-1)%%ppp)==0){
+			mtext(title, side=3, outer=T, cex=1.2, font=2);
+		}
+	}
+
+        par(orig.par);
+}
