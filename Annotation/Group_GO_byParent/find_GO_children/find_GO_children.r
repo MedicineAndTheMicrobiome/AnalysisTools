2ee0a9204dc71ce0e3e8fd29de8c722c705a090b
Li <lik2@upmc.edu>
2024-10-03 14:26:01 -0400

    modified to take a list of parent ids from file

--- Annotation/Group_GO_byParent/find_GO_children/find_GO_children.r
+++ Annotation/Group_GO_byParent/find_GO_children/find_GO_children.r
@@ -9,7 +9,7 @@ options(width=120);
 
 params=c(
 	"input", "i", 1, "character",
-	"parent_id", "p", 1, "character",
+	"parent_id_fn", "p", 1, "character",
 	"output_dir", "d", 1, "character"
 );
 
@@ -19,11 +19,12 @@ script_name=unlist(strsplit(commandArgs(FALSE)[4],"=")[1])[2];
 usage = paste(
 	"\nUsage:\n", script_name, "\n",
 	"	-i <input OBO Table/Mapping .tsv file>\n",
-	"	-p <target parent GO ID>\n",
+	"	-p <input target parent GO IDs filename>\n",
 	"	-o <output directory>\n",
 	"\n",
 	"This script will:\n",
-	"  1.) Take the specified GO parent ID and look up it's immediate children. \n",
+	"  1.) Read in the parent GO IDs from the target parent file.\n",
+	"  2.) For each GO parent ID, look up it's immediate children. \n",
 	"  2.) For each immediate child, all it's decendants will be identified.\n",
 	"\n",
 	"A set of files will be created.\n",
@@ -37,12 +38,12 @@ usage = paste(
 	"\n",
 	"	 A single child of the specified parent ID will have multiple descendants.\n",
 	"\n",
-	"Both of these files will be placed into the specified output directory.\n",
+	"All of these files will be placed into the specified output directory.\n",
 	"\n");
 
 if(
 	!length(opt$input) |
-	!length(opt$parent_id) |
+	!length(opt$parent_id_fn) |
 	!length(opt$output_dir)
 ){
 	cat(usage);
@@ -50,19 +51,16 @@ if(
 }
 
 InputOBOMap=opt$input;
-ParentID=opt$parent_id;
+ParentIDFilename=opt$parent_id_fn;
 OutputDir=opt$output_dir;
 
 ##############################################################################
 
-cln_pid=gsub(":", "_", ParentID);
 
 cat("\n");
-cat("Targeted Parent: ", ParentID, "\n");
+cat("Targeted Parent ID Filename: ", ParentIDFilename, "\n");
 cat("OBO Map Name: ", InputOBOMap, "\n");
 cat("Output Dir: ", OutputDir, "\n");
-cat("\n");
-cat("Clean Parent ID: ", cln_pid, "\n");
 cat("\n");
 
 if(!dir.exists(OutputDir)){
@@ -74,15 +72,26 @@ if(!dir.exists(OutputDir)){
 
 ##############################################################################
 
+load_list=function(fn){
+	dat=read.table(fn, header=F, sep="\t", quote=NULL, comment.char="#");
+	return(dat[,1]);
+}
+
+##############################################################################
+
 load_ids_to_names=function(obo_tab){
 
 	cat("Loading ID to Name Map...\n");
+	start_time=Sys.time();
 	id_to_name_map=list();
 	num_rows=nrow(obo_tab);
 	for(i in 1:num_rows){
 		id_to_name_map[[obo_tab[i, "id"]]]=obo_tab[i, "name"];
 	}
+	end_time=Sys.time();
 	cat("ok.\n");
+	exec_time=end_time-start_time;
+	cat("Time: ", exec_time, " secs\n", sep="");
 	return(id_to_name_map);
 }
 
@@ -210,51 +219,6 @@ load_obo_map=function(obo_map_fn){
 }
 
 ##############################################################################
-##############################################################################
-
-obo_mat=load_obo_map(InputOBOMap);
-id_to_name_map=load_ids_to_names(obo_mat);
-go_tree=load_obo_to_tree(obo_mat);
-
-parent_id_name=lookup_name(ParentID, id_to_name_map);
-cat("\n\n");
-cat("Name of Parent ID: ", ParentID, ":\n", sep="");
-cat("\"", parent_id_name, "\"\n", sep="");
-cat("\n");
-
-immed_children_arr=find_immediate_children(ParentID, go_tree);
-num_immed_children=length(immed_children_arr);
-
-cat("Num Immediate Children: ", num_immed_children, "\n");
-print(immed_children_arr);
-cat("\n");
-
-# Foreach immediate child look up descendants.
-
-groupings=list();
-
-# Include parents for a placeholder
-groupings[[ParentID]]=ParentID;
-
-for(imm_child in immed_children_arr){
-
-	cat("Looking up Immediate Child: ", imm_child, "\n");	
-	start_time=Sys.time();
-
-	# Include immediate child for placeholder
-	descendants=find_all_descendants(imm_child, go_tree);
-	end_time=Sys.time();
-	search_time=end_time-start_time;
-
-	num_desc=length(descendants);
-	cat("Num of Descendants: ", num_desc, "\n", sep="");
-	cat("Search time: ", search_time, " secs.\n", sep="");
-
-	groupings[[imm_child]]=c(imm_child, descendants);
-
-}
-
-##############################################################################
 
 write_descriptions=function(fname, grps, lookup_map){
 
@@ -262,25 +226,22 @@ write_descriptions=function(fname, grps, lookup_map){
 	grp_names=names(grps);
 	num_names=length(grp_names);
 
-	colnm=c("GO_ID", "Num_Members", "Name");
+	colnm=c("GO_ID", "Name", "[Num_Members]");
 	name_matrix=matrix("", nrow=num_names, ncol=length(colnm));
 	colnames(name_matrix)=colnm;
 	
 	for(i in 1:num_names){
 		num_members=length(grps[[grp_names[i]]]);
-		name_matrix[i,]=c(grp_names[i], num_members, lookup_name(grp_names[i], lookup_map));
+		name_matrix[i,]=c(
+			grp_names[i], 
+			lookup_name(grp_names[i], lookup_map), 
+			paste("[", num_members, "]", sep=""));
 	}
 
 	write.table(name_matrix, fname, row.names=F, col.names=T, quote=F);
 	cat("ok.\n");
 	
 }
-
-##############################################################################
-
-cat("\n");
-child_names_fn=paste(OutputDir, "/", cln_pid, ".immediate_descriptions.tsv", sep="");
-write_descriptions(child_names_fn, groupings, id_to_name_map);
 
 ##############################################################################
 
@@ -303,18 +264,93 @@ write_parent_child_map=function(fname, grps, lookup_map){
 		out_mat[,2]=ids;
 		out_mat[,3]=lookup_name(ids, lookup_map);
 	
-		cat("Writing: ", nm, "\n");
+		cat("\tWriting descendants for: ", nm, "\n");
 		write.table(out_mat, fname, row.names=F, col.names=F, quote=F, append=T);
 	}
 	cat("ok.\n");
 
 }
 
-cat("\n");
-imm_child_to_desc_map_fn=paste(OutputDir, "/", cln_pid, ".groupings.map", sep="");
-write_parent_child_map(imm_child_to_desc_map_fn, groupings, id_to_name_map);
+##############################################################################
+
+extract_descendants=function(pid, parent_to_child_tree, lookup_map){
+
+
+}
 
 ##############################################################################
+# Load inputs
+
+target_parent_ids=load_list(ParentIDFilename);
+num_target_parent_ids=length(target_parent_ids);
+
+cat("Target Parent IDs [", num_target_parent_ids, "]:\n");
+print(target_parent_ids);
+cat("\n");
+
+obo_mat=load_obo_map(InputOBOMap);
+id_to_name_map=load_ids_to_names(obo_mat);
+go_tree=load_obo_to_tree(obo_mat);
+
+for(pidx in target_parent_ids){
+
+	cat("-------------------------------------------------------------\n");
+
+	parent_id_name=lookup_name(pidx, id_to_name_map);
+	cat("\n");
+	cat("Name of Parent ID: ", pidx, ":\n", sep="");
+	cat("\t\"", parent_id_name, "\"\n", sep="");
+	cat("\n");
+
+	# Lookup immediate children
+	immed_children_arr=find_immediate_children(pidx, go_tree);
+	num_immed_children=length(immed_children_arr);
+
+	cat("Num Immediate Children: ", num_immed_children, "\n");
+	print(immed_children_arr);
+	cat("\n");
+
+	# Foreach immediate child look up descendants.
+	# Include parents for a placeholder
+	groupings=list();
+	groupings[[pidx]]=pidx;
+
+	for(imm_child in immed_children_arr){
+
+		cat("Looking up Immediate Child: ", imm_child, "\n");	
+		start_time=Sys.time();
+
+		# Include immediate child for placeholder
+		descendants=find_all_descendants(imm_child, go_tree);
+		end_time=Sys.time();
+		search_time=end_time-start_time;
+
+		num_desc=length(descendants);
+		cat("  Num of Descendants: ", num_desc, "\n", sep="");
+		cat("  Search time: ", search_time, " secs.\n", sep="");
+
+		groupings[[imm_child]]=c(imm_child, descendants);
+
+	}
+
+	##############################################################################
+
+	cat("\n");
+
+	cln_pid=gsub(":", "_", pidx);
+
+	# Write descriptions
+	child_names_fn=paste(OutputDir, "/", cln_pid, ".immediate_descriptions.tsv", sep="");
+	write_descriptions(child_names_fn, groupings, id_to_name_map);
+
+	# Write mappings
+	imm_child_to_desc_map_fn=paste(OutputDir, "/", cln_pid, ".groupings.map", sep="");
+	write_parent_child_map(imm_child_to_desc_map_fn, groupings, id_to_name_map);
+
+	##############################################################################
+
+	cat("-------------------------------------------------------------\n");
+}
 
 
 cat("\nDone.\n");
