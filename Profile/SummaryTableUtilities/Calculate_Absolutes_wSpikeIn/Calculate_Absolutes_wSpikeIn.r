64e6e3f6a54de0bb8dc937150695390d13667e53
Li <lik2@upmc.edu>
2025-05-30 18:44:41 -0400

    added sample counts annotation and moved spaghetti plots inside conditional

--- Profile/SummaryTableUtilities/Calculate_Absolutes_wSpikeIn/Calculate_Absolutes_wSpikeIn.r
+++ Profile/SummaryTableUtilities/Calculate_Absolutes_wSpikeIn/Calculate_Absolutes_wSpikeIn.r
@@ -307,12 +307,24 @@ for(cur_ref_nm in c(reference_names, "_combined_")){
 			return(c(span[1]-margin, span[2]+margin));	
 		}
 
-		hist(data,
+		h=hist(data,
 			main="", col=barcol,
 			xlim=widen(hxlim, 1.1), ylim=hylim*1.1, breaks=hbreaks,
 			xlab="Log10(Copies)");
+
+		#print(data);
+		#print(h$counts);
+		#print(sum(h$counts));
+		num_nan=sum(is.nan(data));
+
+		text(hxlim[1]/2, hylim[2]/2, 
+			paste("[Num NaN]\n", format(round(num_nan,0), big.mark=","), sep=""), 
+			font=2, col="gray20");
+
 		title(main=title, line=3, cex.main=1.5, font.main=2);
-		title(main=subtitle, line=1.7, cex.main=1);
+
+		subtit_smpcnt_str=paste(subtitle, ": Num Samples = ", length(data), sep="");
+		title(main=subtit_smpcnt_str, line=1.7, cex.main=1);
 		title(main=paste("--- Ref Log10(Copy Number)") , line=.7, cex.main=.85);
 
 		abline(v=median, col="orange", lwd=3);
@@ -383,8 +395,6 @@ for(cur_ref_nm in c(reference_names, "_combined_")){
 	ref_itn=ref_itn+1;
 }
 
-cat("--------------------------------------------------\n");
-
 ##############################################################################
 
 output_sample_copy_count_summary=function(samp_info_rec, outfn, smp_dep){
@@ -454,11 +464,17 @@ generate_sample_prof_line_plot=function(samp_info_rec){
 	combined_abvchar=samp_info_rec[["_combined_"]][["RefAbdAboveCutoff"]];
 	combined_abv_char=rep(1, num_samples);
 	combined_abv_char[!combined_abvchar]=4;
+
+	# Sum up the NaN so we can annotate the figure
+	nan_count_arr=numeric(length(ref_names));
+	names(nan_count_arr)=ref_names;
 	
 	# Find ranges of copy counts across datasets
 	max_est=0;
 	for(ref_nm in names(samp_info_rec)){
-		max_est=max(max_est, samp_info_rec[[ref_nm]][["TotalCopyCounts"]], na.rm=T);
+		tcc=samp_info_rec[[ref_nm]][["TotalCopyCounts"]];
+		max_est=max(max_est, tcc, na.rm=T);
+		nan_count_arr[ref_nm]=sum(is.nan(tcc));
 	}
 	log10_max_est=log10(max_est);
 
@@ -474,6 +490,7 @@ generate_sample_prof_line_plot=function(samp_info_rec){
 	plot(0, type="n", ylim=c(0, max_est*1.1), xlim=c(0,num_samples+1), las=2,
 		xlab="Sample", ylab="Estimated Sample Copy Count", 
 		main="Variability in Estimated Copy Counts");
+	abline(v=num_samples, col="grey25", lty="dashed");
 
 	ref_ix=1;	
 	for(ref_nm in ref_names){
@@ -495,10 +512,12 @@ generate_sample_prof_line_plot=function(samp_info_rec){
 
 	legend(num_samples/8, max_est,
 		legend=c(ref_names, "COMBINED","", 
-			"  > Min Ref Abnd (reliable)", "  < Min Ref Abnd (dubious)"), 
-		fill=c(ref_col, "black", NA, NA, NA),
-		border=rep(F, 5),
-		pch=c(NA, NA, NA, NA, NA, 1, 4), 
+			"  > Min Ref Abnd (Reliable)", 
+			"  < Min Ref Abnd (Dubious, but computable)",
+			"  Min Ref Abnd = 0 (NaN, not shown)"), 
+		fill=c(ref_col, "black", NA, NA, NA, NA),
+		border=rep(F, 6),
+		pch=c(NA, NA, NA, NA, NA, 1, 4, NA), 
 		cex=.66);
 
 	#------------------------------------------------------------------------
@@ -506,9 +525,13 @@ generate_sample_prof_line_plot=function(samp_info_rec){
 	plot(0, type="n", ylim=c(0, log10_max_est*1.1), xlim=c(0,num_samples+1), las=2,
 		xlab="Sample", ylab="Estimated Log10(Sample Copy Count)", 
 		main="Variability in Log10(Estimated Copy Counts)");
+	abline(v=num_samples, col="grey25", lty="dashed");
 
 	ref_ix=1;	
-	for(ref_nm in ref_names){
+	unrel_count_arr=numeric(length(ref_names)+1);
+	names(unrel_count_arr)=c(ref_names, "_combined_");
+
+	for(ref_nm in c(ref_names, "_combined_")){
 
 		tcc=samp_info_rec[[ref_nm]][["TotalCopyCounts"]];
 		rabvc=samp_info_rec[[ref_nm]][["RefAbdAboveCutoff"]];
@@ -518,11 +541,31 @@ generate_sample_prof_line_plot=function(samp_info_rec){
 		abv_char=rep(1, num_samples);
 		abv_char[!sorted_rabvc]=4;
 
-		points(1:num_samples, log10(sorted_tcc+1), col=ref_ix, pch=abv_char, cex=.5);
+		unrel_count_arr[ref_nm]=sum(!sorted_rabvc);
+
+		if(ref_nm!="_combined_"){
+			points(1:num_samples, log10(sorted_tcc+1), col=ref_ix, pch=abv_char, cex=.5);
+		}
 		ref_ix=ref_ix+1;
 	}
 	points(1:num_samples, log10(combined_cc[sorted_combined_samp_ids]+1), col="black",
 		pch=combined_abv_char, cex=.75);
+
+	cnt_stat=cbind(nan_count_arr, unrel_count_arr);
+
+	computable_but_unreliable=apply(cnt_stat, 1, diff);
+	ref_stat_str=paste(
+		num_samples, " - (", cnt_stat[,2], " = ", cnt_stat[,1], " + ", 
+		computable_but_unreliable, ") = ", num_samples-unrel_count_arr, 
+		sep="");
+
+	legend(num_samples*2/3, log10_max_est, 
+		cex=.66,
+		title="Total - (Dubious = NaN + Computable) = Reliable",
+		fill=c(ref_col, "black"), 
+		legend=c(ref_stat_str),
+		bty="n"
+		);
 
 	text(num_samples/4, 0, "<-- Samples with more CC", col="red", font=4, cex=1);
 	text(num_samples*3/4, 0, "Samples with less CC -->", col="blue", font=4, cex=1);
@@ -530,83 +573,89 @@ generate_sample_prof_line_plot=function(samp_info_rec){
 }
 
 generate_sample_prof_line_plot(sample_info_list);
-quit();
 
 ##############################################################################
 
-cat("Generate spaghetti plots...\n");
+generate_spaghetti_plots=F;
 
-num_samples=nrow(normalized);
+if(generate_spaghetti_plots){
 
-num_color_vals=ceiling(num_samples^(1/3));
-colors=rgb(runif(num_samples), runif(num_samples), runif(num_samples), maxColorValue=1);
+	cat("Generate spaghetti plots...\n");
 
-par(mar=c(15,4,4,1));
-par(mfrow=c(1,1));
+	num_samples=nrow(normalized);
 
-plot(0, type="n", xlim=c(1, num_references), ylim=c(0, 1), xaxt="n", xlab="", las=2,
-	ylab="Abundance",
-	main="Spaghetti Plot: Abundance");
-axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
-for(i in 1:num_samples){
-	points(1:num_references, normalized[i, reference_names], col=colors[i], type="l"); 
-}
+	num_color_vals=ceiling(num_samples^(1/3));
+	colors=rgb(runif(num_samples), runif(num_samples), runif(num_samples), maxColorValue=1);
 
-plot(0, type="n", xlim=c(1, num_references), ylim=c(log10_min_nz, 0), xaxt="n", xlab="", las=2,
-	ylab="log10(Abundance)",
-	main="Spaghetti Plot: Log10(Abundance)");
-axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
-for(i in 1:num_samples){
-	points(1:num_references, log10(normalized[i, reference_names]+min_nz), col=colors[i], type="l"); 
-}
+	par(mar=c(15,4,4,1));
+	par(mfrow=c(1,1));
 
-plot(0, type="n", xlim=c(1, num_references), ylim=c(1, num_samples), xaxt="n", xlab="", las=2,
-	ylab="Rank(Abundance)",
-	main="Spaghetti Plot: Rank(Abundance)");
-axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
-rank_norm=apply(normalized[,reference_names], 2, rank);
-colnames(rank_norm)=reference_names;
-for(i in 1:num_samples){
-	points(1:num_references, rank_norm[i, reference_names], col=colors[i], type="l"); 
-}
+	plot(0, type="n", xlim=c(1, num_references), ylim=c(0, 1), xaxt="n", xlab="", las=2,
+		ylab="Abundance",
+		main="Spaghetti Plot: Abundance");
+	axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
+	for(i in 1:num_samples){
+		points(1:num_references, normalized[i, reference_names], col=colors[i], type="l"); 
+	}
 
-###############################################################################
+	plot(0, type="n", xlim=c(1, num_references), ylim=c(log10_min_nz, 0), xaxt="n", xlab="", las=2,
+		ylab="log10(Abundance)",
+		main="Spaghetti Plot: Log10(Abundance)");
+	axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
+	for(i in 1:num_samples){
+		points(1:num_references, log10(normalized[i, reference_names]+min_nz), 
+			col=colors[i], type="l"); 
+	}
 
-cat("Calculate Correlations among references...\n");
-spearmans_cor=cor(normalized[, reference_names]);
+	plot(0, type="n", xlim=c(1, num_references), ylim=c(1, num_samples), xaxt="n", xlab="", las=2,
+		ylab="Rank(Abundance)",
+		main="Spaghetti Plot: Rank(Abundance)");
+	axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
+	rank_norm=apply(normalized[,reference_names], 2, rank);
+	colnames(rank_norm)=reference_names;
+	for(i in 1:num_samples){
+		points(1:num_references, rank_norm[i, reference_names], col=colors[i], type="l"); 
+	}
 
-par(mar=c(7,3,5,7));
-paint_matrix(spearmans_cor, "Rank Correlation between References", plot_min=0, plot_max=1,
-	deci_pts=3, show_leading_zero=F);
+	###############################################################################
 
-###############################################################################
+	cat("Calculate Correlations among references...\n");
+	spearmans_cor=cor(normalized[, reference_names]);
 
-max_rank_diff=num_samples/10;
+	par(mar=c(7,3,5,7));
+	paint_matrix(spearmans_cor, "Rank Correlation between References", plot_min=0, plot_max=1,
+		deci_pts=3, show_leading_zero=F);
 
-plot(0, type="n", xlim=c(1, num_references), ylim=c(1, num_samples), xaxt="n", xlab="", las=2,
-	ylab="Rank(Abundance)",
-	main=paste("Spaghetti Plot: Rank(Abundance) Increasing > ", max_rank_diff, sep=""));
-axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
-colnames(rank_norm)=reference_names;
-for(i in 1:num_samples){
-	for(r in 1:(num_references-1)){
-		if(diff(rank_norm[i, c(r,r+1)])>max_rank_diff){
-			points(c(r,r+1), rank_norm[i, c(r,r+1)], col=colors[i], type="l"); 
+	###############################################################################
+
+	max_rank_diff=num_samples/10;
+
+	plot(0, type="n", xlim=c(1, num_references), ylim=c(1, num_samples), xaxt="n", xlab="", las=2,
+		ylab="Rank(Abundance)",
+		main=paste("Spaghetti Plot: Rank(Abundance) Increasing > ", max_rank_diff, sep=""));
+	axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
+	colnames(rank_norm)=reference_names;
+	for(i in 1:num_samples){
+		for(r in 1:(num_references-1)){
+			if(diff(rank_norm[i, c(r,r+1)])>max_rank_diff){
+				points(c(r,r+1), rank_norm[i, c(r,r+1)], col=colors[i], type="l"); 
+			}
 		}
 	}
-}
 
-plot(0, type="n", xlim=c(1, num_references), ylim=c(1, num_samples), xaxt="n", xlab="", las=2,
-	ylab="Rank(Abundance)",
-	main=paste("Spaghetti Plot: Rank(Abundance) Decreasing > ", max_rank_diff, sep=""));
-axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
-colnames(rank_norm)=reference_names;
-for(i in 1:num_samples){
-	for(r in 1:(num_references-1)){
-		if(diff(rank_norm[i, c(r,r+1)])< -max_rank_diff){
-			points(c(r,r+1), rank_norm[i, c(r,r+1)], col=colors[i], type="l"); 
+	plot(0, type="n", xlim=c(1, num_references), ylim=c(1, num_samples), xaxt="n", xlab="", las=2,
+		ylab="Rank(Abundance)",
+		main=paste("Spaghetti Plot: Rank(Abundance) Decreasing > ", max_rank_diff, sep=""));
+	axis(side=1, at=1:num_references, labels=reference_names, cex.axis=1, las=2);
+	colnames(rank_norm)=reference_names;
+	for(i in 1:num_samples){
+		for(r in 1:(num_references-1)){
+			if(diff(rank_norm[i, c(r,r+1)])< -max_rank_diff){
+				points(c(r,r+1), rank_norm[i, c(r,r+1)], col=colors[i], type="l"); 
+			}
 		}
 	}
+
 }
 
 ###############################################################################
